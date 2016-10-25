7eab0abaaf32170aed42df6361a19675d1b4ab2b
Robin Schmidt <robin@rs-met.com>
2016-10-25 12:45:58 +0200

    add comments to Filters.h about what to come

--- Libraries/RAPT/Source/Modules/Filters/Filters.h
+++ Libraries/RAPT/Source/Modules/Filters/Filters.h
@@ -6,7 +6,24 @@ namespace RAPT
   
 // make a nested namespace Filters or Filter
 
+//files to come:
+//#include "Basics/OnePole.h"
+//#include "Basics/OnePoleOneZero.h"
+//#include "Basics/TwoPoleOneZero.h"  // decaying sine, etc.
+//#include "Basics/Biquad.h"
+//#include "Basics/BiquadChain.h"
+//...
+
 #include "Musical/LadderFilter.h"
+//include "Musical/StateVariableFilter.h"
+//include "Musical/AttackDecaySineFilter.h" // maybe get rid of writing "Filter" all the time
+
+//files to come:
+//#include "Scientific/Prototypes.h"
+//#include "Scientific/PoleZeroMapping.h" // LP->LP, LP->HP, S->Z bilinear, etc.
+//#include "Scientific/CoefficientConversion.h" // pole-zero-to-biquad, 
+//#include "Scientific/WindowedSinc.h"
+//...
 
 }
 
