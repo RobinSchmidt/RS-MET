b6203919fda7a516662b4c3887ef9e7d59e07856
Robin Schmidt <robin@rs-met.com>
2016-11-27 15:32:30 +0100

    added saturating nonlinearities to PhasorFilter

--- Libraries/RAPT/Source/Modules/Filters/Musical/PhasorFilter.cpp
+++ Libraries/RAPT/Source/Modules/Filters/Musical/PhasorFilter.cpp
@@ -49,13 +49,18 @@ inline void applyNonlinearity(TSig &x, TSig &y)
 
   // coefficients for the nonlinear transformation (make them user adjustable later):
   TSig a = TSig(-0.050); // coeff for same^2
-  TSig b = TSig(-0.050); // coeff for other^2
+  TSig b = TSig(-0.020); // coeff for other^2
   TSig c = TSig(-0.150); // coeff for cross-term same*other
   TSig d = TSig(-0.006); // constant
 
   // apply a nonlinear transformation to the vector:
   x += a*xx + b*yy + c*xy + d;
   y += b*xx + a*yy + c*xy + d;
+
+  // saturation 1:
+  TSig sat1 = TSig(0.3);   // higher values introduce a pre-gap in resonance
+  x /= 1 + sat1*x*x;
+  y /= 1 + sat1*y*y;
 
   // restore old vector length (renormalization):
   TSig R2 = x*x + y*y;      // new length
@@ -66,6 +71,12 @@ inline void applyNonlinearity(TSig &x, TSig &y)
     y *= s;
   }
   // maybe we should have different renormalization modes: never, always, if R2 > r2, etc.
+
+  // saturation 2:
+  TSig sat2 = TSig(0.005); // higher values shorten the resonance
+  TSig s = 1 / (1 + sat2 *  (x*x + y*y));
+  x *= s;
+  y *= s;
 
   // Maybe it's possible to use some function that doesn't require explicit renormalization. If 
   // we use xNew = x + f(x,y), yNew = y + g(x,y) for the application of the nonlinearity, I think
@@ -82,7 +93,10 @@ inline void PhasorFilter<TSig, TPar>::processFrame(TSig *x, TSig *y)
   *x  += Axx * xOld  +  Axy * yOld;
   *y  += Ayx * xOld  +  Ayy * yOld;
 
-  applyNonlinearity(*x, *y);  // experimental
+  applyNonlinearity(*x, *y);  
+   // experimental - todo: have as member a pointer to an "Mapper2D" object that can be set up 
+   // from outside - then later, we ma call here mapper->map(*x, *y). Depending on what subclass
+   // of Mapper2D the client code passes, we have different distortion modes.
 
   xOld = *x;
   yOld = *y;
@@ -94,7 +108,9 @@ inline TSig PhasorFilter<TSig, TPar>::getSample(TSig in)
   TSig x = in;
   TSig y = 0;
   processFrame(&x, &y);
-  return x;  // maybe use weighted sum of in, x and y according to output coeffs
+  //return x;    // maybe use weighted sum of in, x and y according to output coeffs
+  //return y;
+  return x + y;  // test
 }
 
 // misc:
@@ -123,4 +139,9 @@ void PhasorFilter<TSig, TPar>::updateCoefficients()
   // This computation assumes a pair of complex conjugate poles. Maybe we can come up with a 
   // different formula for the matrix coefficients that realizes a pair of real poles such that 
   // this filter can emulate a general biquad (transfer function wise).
+
+  // todo: maybe keep the pole radius as member and let the user set it up directly ...maybe we
+  // should factor out a baseclass PhasorFilterBase that only has pole radius r and normalized 
+  // radian frequency w as parameters. we could then also use r > 1 (of course only, if some
+  // saturating nonlinearity is present, otherwise it will explode)
 }
