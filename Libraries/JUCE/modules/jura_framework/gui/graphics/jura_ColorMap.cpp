466980a381b72396a424194575bdd33606948c9d
Robin Schmidt <robin@rs-met.com>
2017-01-13 18:58:00 +0100

    populated ColorMap class

--- Libraries/JUCE/modules/jura_framework/gui/graphics/jura_ColorMap.cpp
+++ Libraries/JUCE/modules/jura_framework/gui/graphics/jura_ColorMap.cpp
@@ -7,15 +7,59 @@ ColorMap::ColorMap()
 
 void ColorMap::setFromColourGradient(const ColourGradient &g)
 {
-
+  gradient = g;
+  updateArray();
 }
 
 void ColorMap::setDefaultMap(int index)
 {
-
+  setFromColourGradient(getDefaultGradient(index));
 }
 
-//void ColorMap::setSize(int newSize)
-//{
-//
-//}
+void ColorMap::setSize(int newSize)
+{
+  colors.resize(newSize);
+  lastIndex = newSize-1;
+  updateArray();
+}
+
+ColourGradient ColorMap::getDefaultGradient(int index)
+{
+  juce::ColourGradient g;
+
+  switch(index)
+  {
+  case fire:
+  {
+    g.addColour(0.0, Colour(  0,   0,   0));  // black
+    g.addColour(0.4, Colour(255,   0,   0));  // red
+    g.addColour(0.6, Colour(255, 255,   0));  // yellow
+    g.addColour(1.0, Colour(255, 255, 255));  // white
+  } break;
+  case ice:
+  {
+    g.addColour(0.0, Colour(  0,   0,   0));  // black
+    g.addColour(0.4, Colour(  0,   0, 255));  // blue
+    g.addColour(0.6, Colour(  0, 255, 255));  // cyan
+    g.addColour(1.0, Colour(255, 255, 255));  // white
+  } break;
+  default: // gray-scale is the default map
+  {
+    g.addColour(0.0, Colour(  0,   0,   0));  // black
+    g.addColour(1.0, Colour(255, 255, 255));  // white
+  }
+  }
+
+  return g;
+}
+
+void ColorMap::updateArray()
+{
+  Colour c;
+  double scaler = 1.0 / lastIndex;
+  for(int i = 0; i < colors.size(); i++)
+  {
+    c = gradient.getColourAtPosition(scaler * i);
+    colors[i] = c.getARGB();
+  }
+}
--- Libraries/JUCE/modules/jura_framework/gui/graphics/jura_ColorMap.h
+++ Libraries/JUCE/modules/jura_framework/gui/graphics/jura_ColorMap.h
@@ -1,14 +1,20 @@
 #ifndef jura_ColorMap_h
 #define jura_ColorMap_h
 
-/** A class for .... */
+/** A class for representing a colormap that is used for visualizing 2D data. It is based on a 
+juce::ColourGradient object, but allows for (much!) faster access of the individual colors in the
+gradient by creating an internal array of pre-interpolated colors. So, basically, it's a lookup 
+table for a ColourGradient object. 
+
+\todo: provide methods for conversion from/to xml
+*/
 
 class JUCE_API ColorMap
 {
 
 public:
 
-  /** Enumeration of the hard coded default maps. */
+  /** Enumeration of the hard coded predefined maps. */
   enum defaultMaps
   {
     grayScale,
@@ -29,8 +35,8 @@ public:
   @see defaultMaps */
   void setDefaultMap(int index);
 
-  ///** Sets the size of the array. Larger sizes will give smoother color transitions. */
-  //void setSize(int newSize);
+  /** Sets the size of the array. Larger sizes will give smoother color transitions. */
+  void setSize(int newSize);
 
 
   /** \name Color retrieval */
@@ -44,11 +50,22 @@ public:
   }
 
 
+  /** \name Misc */
+
+  /** Returns one of the default color gradients as juce::ColourGradient object. */
+  ColourGradient getDefaultGradient(int index);
+
 protected:
 
-  
+  /** Updates our internally stored array of colors according to the gradient member. */
+  void updateArray();
+
+
   std::vector<uint32> colors;
   int lastIndex;
+
+  ColourGradient gradient; // the juce color gradient object on which this map is based
+
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ColorMap)
 };
