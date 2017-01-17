4b2c42983331cc5aee90b79c8130fb41367809e0
Robin Schmidt <robin@rs-met.com>
2017-01-17 15:13:45 +0100

    added stuff to ImagePainter

--- Libraries/RAPT/Source/Modules/Visualization/Graphics/ImagePainter.cpp
+++ Libraries/RAPT/Source/Modules/Visualization/Graphics/ImagePainter.cpp
@@ -5,3 +5,35 @@ ImagePainter<TPix, TWgt, TCor>::ImagePainter(Image<TPix> *imageToPaintOn, Image<
   brush = brushToUse;
 }
 
+// setup
+
+template<class TPix, class TWgt, class TCor>
+void ImagePainter<TPix, TWgt, TCor>::setImageToPaintOn(Image<TPix> *imageToPaintOn)
+{
+  image = imageToPaintOn;
+}
+
+template<class TPix, class TWgt, class TCor>
+void ImagePainter<TPix, TWgt, TCor>::setBrushToUse(Image<TWgt> *brushToUse)
+{
+  brush = brushToUse;
+}
+
+// painting
+
+template<class TPix, class TWgt, class TCor>
+void ImagePainter<TPix, TWgt, TCor>::paintDot(TCor x, TCor y, TPix color)
+{
+  int wi = image->getWidth();
+  int hi = image->getHeight();
+  int wb = brush->getWidth();
+  int hb = brush->getHeight();
+
+  // ...something to do...
+  // we need a (nested) loop over all (x,y) pixels in the brush, multiply the brush value there 
+  // with the color accumulate it into the corresponding location in the target image using 
+  // bilinear deinterpolation to write into fractional positions
+  // or: loop over a rectangle of size wb,wh in the target image and read out the brush at 
+  // corresponding positions using bilinear interpolation ...maybe provide both methods so we
+  // can compare the results (should they be equal? idk) and also the performance
+}
--- Libraries/RAPT/Source/Modules/Visualization/Graphics/ImagePainter.h
+++ Libraries/RAPT/Source/Modules/Visualization/Graphics/ImagePainter.h
@@ -1,7 +1,13 @@
 #ifndef RAPT_IMAGEPAINTER_H_INCLUDED
 #define RAPT_IMAGEPAINTER_H_INCLUDED
 
-/**  */
+/** A class for painting on an Image object. It is based on a second image that we use as "brush".
+Whenever a "dot" is painted onto the image at a particular location, a copy of the brush image will
+be blended into the target image at that location. The pixel types for the target image and the 
+brush image can in general be different - for example the target image could use a RBGA type 
+represented by a vector of 4 floats and the brush could use just a single float. The brush pixels
+will be used as weights by which a new color will be multiplied when it is accumulated into the 
+image. */
 
 template<class TPix, class TWgt, class TCor>  // pixel, weight, coordinate types
 class ImagePainter
@@ -15,14 +21,12 @@ public:
   ImagePainter(Image<TPix> *imageToPaintOn = nullptr, Image<TWgt> *brushToUse = nullptr);
 
 
-
-
   /** \name Setup */
 
-  /**  */
+  /** Sets the image on which we paint.  */
   void setImageToPaintOn(Image<TPix> *imageToPaintOn);
 
-
+  /** Sets the brush that we use. It is basically a matrix of weights. */
   void setBrushToUse(Image<TWgt> *brushToUse);
 
 
