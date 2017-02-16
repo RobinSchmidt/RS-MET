352c0619ffc7574bf32337b3ec923fdca23f5a3b
Robin Schmidt <robin@rs-met.com>
2017-02-16 11:17:19 +0100

    prepared lineDrawing test function for Wu lines

--- Libraries/RAPT/Source/Executables/Experiments/GraphicsExperiments.cpp
+++ Libraries/RAPT/Source/Executables/Experiments/GraphicsExperiments.cpp
@@ -1,5 +1,10 @@
 #include "GraphicsExperiments.h"
 
+void drawLineWu(ImageF& image, float x1, float y1, float x2, float y2, float color)
+{
+  // add Wu algorithm from wikipedia here
+}
+
 void lineDrawing()
 {
   // Compares different line drawing algorithms. We draw lines of different directions.
@@ -16,24 +21,31 @@ void lineDrawing()
   ImagePainterFFF painter(&image, nullptr);
   float x1 = margin;
   float y1 = margin;
-  float x2, y2;
+  //float x2, y2;
   int i;
 
+  // create arrays for line endpoints:
+  vector<float> xE, yE;
+  for(i = 0; i < numLines; i++){    // flat, horizontal'ish
+    xE.push_back(imageWidth - margin);
+    yE.push_back(margin + i * (imageHeight - margin) / numLines);
+  }
+  xE.push_back(imageWidth -margin); // 45° diagonal
+  yE.push_back(imageHeight-margin);
+  for(i = 0; i < numLines; i++){    // steep, vertical'ish
+    xE.push_back(margin + i * (imageWidth - margin) / numLines);
+    yE.push_back(imageHeight - margin);
+  }
+
   // draw lines using the algorithm that creates them from dots:
-  for(i = 0; i < numLines; i++){ // horizontal'ish
-    x2 = imageWidth - margin;
-    y2 = margin + i * (imageHeight - margin) / numLines;
-    painter.drawDottedLine(x1, y1, x2, y2, brightness);
-  }
-  for(i = 0; i < numLines; i++){ // vertical'ish
-    y2 = imageHeight - margin;
-    x2 = margin + i * (imageWidth - margin) / numLines;
-    painter.drawDottedLine(x1, y1, x2, y2, brightness);
-  }
-  painter.drawDottedLine(x1, y1, imageWidth-margin, imageHeight-margin, brightness);
+  for(i = 0; i < xE.size(); i++)
+    painter.drawDottedLine(x1, y1, xE[i], yE[i], brightness);
   writeImageToFilePPM(image, "DottedLines.ppm");
 
-
-  int dummy = 0; 
+  // draw lines with Wu algorithm:
+  image.clear();
+  for(i = 0; i < xE.size(); i++)
+    drawLineWu(image, x1, y1, xE[i], yE[i], brightness);
+  writeImageToFilePPM(image, "WuLines.ppm");
 }
 
