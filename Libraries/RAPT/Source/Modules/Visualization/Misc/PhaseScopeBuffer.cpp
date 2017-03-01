0da2a3c394443ff134ba9d45cb84015583217cee
Robin Schmidt <robin@rs-met.com>
2017-03-02 00:57:17 +0100

    PhaseScopeBuffer: changed weight formula for small dots

--- Libraries/RAPT/Source/Modules/Visualization/Misc/PhaseScopeBuffer.cpp
+++ Libraries/RAPT/Source/Modules/Visualization/Misc/PhaseScopeBuffer.cpp
@@ -80,7 +80,8 @@ template<class TSig, class TPix, class TPar>
 void PhaseScopeBuffer<TSig, TPix, TPar>::setPixelSpread(TPar newSpread)
 {
   thickness = newSpread;
-  painter.setNeighbourWeightsForSimpleDot((TSig)thickness, 0.5f*thickness);
+  painter.setNeighbourWeightsForSimpleDot((TSig)thickness, thickness*thickness);
+  //painter.setNeighbourWeightsForSimpleDot((TSig)thickness, 0.5f*thickness);
 }
 
 template<class TSig, class TPix, class TPar>
