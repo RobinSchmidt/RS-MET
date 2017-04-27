2f40a29da40da60741f3b1f2510e6bb1a656d051
Robin Schmidt <robin@rs-met.com>
2017-04-27 21:15:53 +0200

    changed "INLINE" definition for gcc from "__attribute__((always_inline))" to "inline" to fix "multiple definition" linker errors on gcc/win

--- Libraries/JUCE/modules/rosic/basics/GlobalDefinitions.h
+++ Libraries/JUCE/modules/rosic/basics/GlobalDefinitions.h
@@ -10,8 +10,8 @@ rosic namespace to facilitate their global use. */
 #ifdef _MSC_VER
 #define INLINE __forceinline
 #else
-//#define INLINE inline
-#define INLINE __attribute__((always_inline))
+#define INLINE inline
+//#define INLINE __attribute__((always_inline))
 #endif
 
 
@@ -130,19 +130,21 @@ typedef unsigned __int32 UINT32;
 //#define INF (*(reinterpret_cast<double *>(&INF_ULL)))
 //#define NEG_INF (*(reinterpret_cast<double *>(&NEG_INF_ULL)))
 
-INLINE double dummyFunction(double x) { return x; }
 #ifdef _MSC_VER
+  INLINE double dummyFunction(double x) { return x; }
   #define INF (1.0/dummyFunction(0.0))
   #define NEG_INF (-1.0/dummyFunction(0.0))  // get rid of that - use -INF in the code
   //#ifndef NAN
   //  #define NAN (dummyFunction(0.0)/dummyFunction(0.0))
   //#endif
+  #define INDEF (dummyFunction(0.0) / dummyFunction(0.0))
 #else
   #define INF (1.0/0.0)
   #define NEG_INF (-1.0/0.0)
+  #define INDEF (0.0/0.0)
 #endif
 
-#define INDEF (dummyFunction(0.0) / dummyFunction(0.0))
+
 
 //-------------------------------------------------------------------------------------------------
 // debug stuff:
