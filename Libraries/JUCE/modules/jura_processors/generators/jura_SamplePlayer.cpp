d2f003fbd9a7faa3fdc02d6a34c9d97210495025
Robin Schmidt <robin@rs-met.com>
2017-05-06 16:56:02 +0200

    added stuff to SamplePlayer.h/cpp

--- Libraries/JUCE/modules/jura_processors/generators/jura_SamplePlayer.cpp
+++ Libraries/JUCE/modules/jura_processors/generators/jura_SamplePlayer.cpp
@@ -353,3 +353,440 @@ void SamplePlayerAudioModule::initializeAutomatableParameters()
     parameterChanged(parameters[i]);
 }
 
+//=================================================================================================
+
+SamplePlayerEditorDisplay::SamplePlayerEditorDisplay(AudioFileBuffer *newBufferToUse) 
+  : WaveformDisplay(newBufferToUse) //, InteractiveCoordinateSystem(juce::String(T("SampleDisplay")))
+{
+
+  setValueFieldPopup(false);
+
+  //bufferToEdit             = NULL;
+  //playbackParametersToEdit = NULL;
+  samplePlayerToEdit = NULL;
+  locatorBeingDragged      = -1;
+
+  setAxisLabelX(juce::String::empty);
+  setAxisLabelY(juce::String::empty);
+  setAxisPositionX(CoordinateSystem::INVISIBLE);
+  setAxisPositionY(CoordinateSystem::INVISIBLE);
+
+  /*
+  lockUsedBufferPointer();
+  bufferToUse = &buffer;
+  unlockUsedBufferPointer();
+  */
+  assignAudioFileBuffer(&buffer);
+
+  //buffer.registerUser(this);
+  //scaleFactor          = 1.0f;
+  //dcOffset             = 0.0f;
+}
+
+SamplePlayerEditorDisplay::~SamplePlayerEditorDisplay()
+{
+  assignAudioFileBuffer(NULL);
+  //buffer.deRegisterUser(this);
+  //deleteAllChildren();
+}
+
+
+//-------------------------------------------------------------------------------------------------
+// 
+
+/*
+CoordinateSystemRangeOld SamplePlayerEditorDisplay::getMaximumMeaningfulRange(
+double relativeMarginLeft, double relativeMarginRight, 
+double relativeMarginTop,  double relativeMarginBottom)
+{
+return WaveformDisplayOld::getMaximumMeaningfulRange(relativeMarginLeft, relativeMarginRight,
+relativeMarginTop, relativeMarginBottom);
+}
+*/
+
+void SamplePlayerEditorDisplay::setSamplePlayerToEdit(rosic::SamplePlayer *newPlayerToEdit)
+{
+  samplePlayerToEdit = newPlayerToEdit;
+
+  if( samplePlayerToEdit == NULL )
+    return;
+
+  // update the waveform display:
+  int numSamples  = samplePlayerToEdit->getNumSamples();
+  int numChannels = samplePlayerToEdit->getNumChannels();
+  setMaximumRange(0.0, jmax((double)numSamples,1.0), -1.1, 1.1);
+  setCurrentRange(0.0, jmax((double)numSamples,1.0), -1.1, 1.1);
+
+
+  //buffer.setDataToReferTo( samplePlayerToEdit->getSampleData(), numSamples, numChannels );
+
+  /*
+  lockUsedBufferPointer();
+  if( bufferToUse != NULL )
+  {
+  juce::String audioFile = getApplicationDirectory() + File::separatorString 
+  + juce::String(samplePlayerToEdit->getSampleName());
+  bufferToUse->loadAudioDataFromFile(audioFile, true);
+  WaveformDisplay::setDirty();
+  repaint(); // should not be necesarry?
+  }
+  unlockUsedBufferPointer();
+  */
+
+
+  juce::String audioFile = getApplicationDirectory() + juce::File::separatorString   
+    + juce::String(samplePlayerToEdit->getSampleName());
+  setAudioFileToUse(juce::File(audioFile));
+
+  //setWaveform(samplePlayerToEdit->getSampleData(), numSamples, numChannels );
+}
+
+bool SamplePlayerEditorDisplay::setAudioFileToUse(const juce::File &newFileToUse)
+{
+  lockUsedBufferPointer();
+  if( bufferToUse != NULL )
+  {
+    bufferToUse->loadAudioDataFromFile(newFileToUse, false);
+    setRangeToBufferLength();
+
+    // pass the data to the audio core-object:
+    if( samplePlayerToEdit != NULL )
+    {
+      bufferToUse->acquireReadLock();
+
+      // todo: retrieve the pointers to the channels and pass it to the rosic-object, this might 
+      // also be the place to render samples from modal models etc.
+
+      //bufferToUse->getSampleData(0, 0);
+      //samplePlayerToEdit->setSampleData(channelPointers, numSamples numChannles);
+
+      samplePlayerToEdit->parameters->setRecordingSampleRate(bufferToUse->getFileSampleRate());
+
+      bufferToUse->releaseReadLock();
+    }
+
+    //setMaximumRangeX(0.0, bufferToUse->getNumSamples()+1);
+    //setCurrentRangeX(0.0, bufferToUse->getNumSamples()+1);
+    setDirty();
+    //updatePlot();
+    //repaint(); // should not be necesarry?
+  }
+  unlockUsedBufferPointer();
+
+  return true; // preliminary
+}
+
+/*
+void SamplePlayerEditorDisplay::setSamplePlaybackParametersToEdit(
+rosic::SamplePlaybackParameters *newParametersToEdit)
+{
+playbackParametersToEdit = newParametersToEdit;
+}
+*/
+
+/*
+bool SamplePlayerEditorDisplay::setWaveform(double** newWaveformData, int newNumSampleFrames, 
+int newNumChannels)
+{
+bool success = false;
+
+if( modulatorToEdit == NULL )
+return false;
+
+// call the setWavefrom-function from the WavefromDisplay base-class - this will result in a 
+// copying of the passed data into the (inherited) member 'peakData' 
+// (which is pointer to float):
+success = WaveformDisplay::setWaveform(newWaveformData, newNumSampleFrames, newNumChannels);
+
+// the float* peakData member now contains the data (plus possibly some padding and decimated 
+// data but thsi additional stuff will come later in the array and can therefore be ignored) 
+// - we can use this pointer to further pass the data through to the rosic::SampleModulator 
+// object which accepts a pointer to float:
+if( success == true )
+modulatorToEdit->sampleModulator.setSampleData(peakData, newNumSampleFrames);
+
+return success;
+}
+
+bool SamplePlayerEditorDisplay::setWaveform(const AudioSampleBuffer& newWaveformBuffer)
+{
+// similar procedure to the function above:
+bool success = false;
+if( modulatorToEdit == NULL )
+return false;
+success = WaveformDisplay::setWaveform(newWaveformBuffer);
+if( success == true )
+modulatorToEdit->sampleModulator.setSampleData(peakData, numSampleFrames);
+
+return success;
+}
+*/
+
+void SamplePlayerEditorDisplay::updatePlot(bool resetZoomFactor)
+{
+  if( samplePlayerToEdit == NULL )
+    return;
+
+  /*
+  // aquire a lock on the sample data - just in case some other thread corrupts it in between calls
+  // getNumSamples/getNumChannels and the actual data-readout via getSampleData():
+  samplePlayerToEdit->lockSampleData();
+
+  // update the waveform display:
+  int numSamples  = samplePlayerToEdit->getNumSamples();
+  int numChannels = samplePlayerToEdit->getNumChannels();
+
+  if( numSamples > 0 && numChannels > 0 )
+  {
+
+  // retrieve the filename (the full path) and cut it down to the filename only:
+  //juce::String fileName = juce::String(bufferToEdit->getSampleName());
+  //if( fileName.contains(T("\\")) )
+  //  fileName = fileName.fromLastOccurrenceOf(T("\\"), false, false);
+  //if( fileName.contains(T("/")) )
+
+  // setup stuff:
+  //sampleDisplay->setCaption(fileName);
+  //setWaveform(samplePlayerToEdit->getSampleData(), numSamples, numChannels );
+  juce::String audioFile = getApplicationDirectory() + File::separatorString 
+  + juce::String(samplePlayerToEdit->getSampleName());
+  bufferToUse->loadAudioDataFromFile(audioFile, true);
+  setMaximumRange(0.0, numSamples, -1.1, 1.1);
+  if( resetZoomFactor == true )
+  setCurrentRange(0.0, numSamples, -1.1, 1.1);
+  }
+  // we may now release the sample data and repaint ourselves:
+  samplePlayerToEdit->unlockSampleData();
+  repaint();
+  */
+
+
+  /*
+  // aquire mutex-locked access to the sample-data (such that no other thread corrupts them while 
+  // we are reading them out):
+  modulatorToEdit->sampleModulator.suspendAudioProcessing();
+  modulatorToEdit->sampleModulator.lockSampleData();
+
+  int      numSamples  = modulatorToEdit->sampleModulator.getNumSamples();
+  double*  sampleData  = modulatorToEdit->sampleModulator.getSampleData();
+  double** sampleData2 = &sampleData; // we actually need a pointer to a pointer
+
+  WaveformDisplay::setWaveform(sampleData2, numSamples, 1);
+
+  // obtain the name (relative path) of the sample-file
+  juce::String fileName = juce::String(modulatorToEdit->sampleModulator.getSampleName());
+
+  // truncate the path-info such that only the file-name will be left:
+  fileName = fileName.fromLastOccurrenceOf(T("/"), false, false);
+  fileName = fileName.fromLastOccurrenceOf(T("\\"), false, false);
+
+  // ...and show the name of the file in the waveform-display:
+  setCaption(fileName);
+
+  // release the mutex-lock:
+  modulatorToEdit->sampleModulator.unlockSampleData();
+  modulatorToEdit->sampleModulator.resumeAudioProcessing();
+  */
+}
+
+void SamplePlayerEditorDisplay::paint(juce::Graphics &g)
+{
+  WaveformDisplay::paint(g); // does not take scale and dc into account
+
+  if( samplePlayerToEdit == NULL )
+    return;
+
+  // draw loop-locators:
+  double factor    = 1.0 / samplePlayerToEdit->parameters->getRecordingSampleRate();
+  double start     = factor * samplePlayerToEdit->parameters->getPlaybackStart();
+  double end       = factor * samplePlayerToEdit->parameters->getPlaybackEnd();
+  double loopStart = factor * samplePlayerToEdit->parameters->getLoopStart();  
+  double loopEnd   = factor * samplePlayerToEdit->parameters->getLoopEnd();
+  double xGrayL    = start;
+  double xGrayR    = end;
+  double y         = 0.0;
+  drawCurrentPositionLocator(g,  (float) start);
+  drawCurrentPositionLocator(g,  (float) end);
+  if( samplePlayerToEdit->parameters->getLoopMode() != rosic::SamplePlaybackParameters::NO_LOOP )
+  {
+    drawLeftLocator( g,  (float) loopStart);
+    drawRightLocator(g,  (float) loopEnd);
+    xGrayL = jmin(start, loopStart);
+  }
+
+
+  // gray out the area before the start sample and after the end sample:
+  g.setColour( Colours::lightgrey.withAlpha(0.5f) );
+  transformToComponentsCoordinates(xGrayL, y);
+  g.fillRect(0.f, 0.f, (float) xGrayL, (float) getHeight() );
+  y = 0.0;
+  transformToComponentsCoordinates(xGrayR, y);
+  g.fillRect((float) xGrayR, 0.f, (float) getWidth(), (float) getHeight() );
+
+  //g.fillAll(Colours::grey.withAlpha(0.5f));
+}
+
+int SamplePlayerEditorDisplay::whatIsUnderTheMouseCursor(const MouseEvent &e)
+{
+  if( samplePlayerToEdit == NULL )
+    return 0;
+
+  // get the position of the event in components coordinates
+  mouseX = e.getMouseDownX();
+  mouseY = e.getMouseDownY();
+
+  // a margin for the breakpoint-dots
+  double marginInPixels = dotRadius;
+
+  // retrieve the scale and offset variables form the Modulator-object
+  double scale  = 1.0;
+  double offset = 0.0;
+
+  double factor = 1.0 / samplePlayerToEdit->parameters->getRecordingSampleRate();
+  double x1 = factor * samplePlayerToEdit->parameters->getPlaybackStart() ;
+  double x2 = factor * samplePlayerToEdit->parameters->getPlaybackEnd() ;
+  double x3 = factor * samplePlayerToEdit->parameters->getLoopStart()     ;
+  double x4 = factor * samplePlayerToEdit->parameters->getLoopEnd()       ;
+  double y1 = 0.0; // dummy
+  double y2 = 0.0; // dummy
+  double y3 = 0.0; // dummy
+  double y4 = 0.0; // dummy
+  transformToComponentsCoordinates(x1, y1);
+  transformToComponentsCoordinates(x2, y2);
+  transformToComponentsCoordinates(x3, y3);
+  transformToComponentsCoordinates(x4, y4);
+
+
+  if( abs(x3+2-mouseX) <= 4.0 && 
+    samplePlayerToEdit->parameters->getLoopMode() != rosic::SamplePlaybackParameters::NO_LOOP )
+  {
+    return LOOP_START_LOCATOR;
+  }
+  else if( abs(x1+2-mouseX) <= 4.0  )
+  {
+    return START_LOCATOR;
+  }
+  else if( abs(x2+2-mouseX) <= 4.0  )
+  {
+    return END_LOCATOR;
+  }
+  else if( abs(x4+2-mouseX) <= 4.0  &&
+    samplePlayerToEdit->parameters->getLoopMode() != rosic::SamplePlaybackParameters::NO_LOOP )
+  {
+    return LOOP_END_LOCATOR;
+  }
+
+  // no object has been identified to be under the mouse cursor:
+  return NO_OBJECT;
+}
+
+void SamplePlayerEditorDisplay::mouseDown(const MouseEvent &e)
+{
+  if( samplePlayerToEdit == NULL )
+    return;
+
+  // get the position of the event in components coordinates
+  mouseX = e.getMouseDownX();
+  mouseY = e.getMouseDownY();
+
+  // check if one of the locators was grabbed:
+  if( e.mods.isLeftButtonDown() )
+  {
+    locatorBeingDragged = whatIsUnderTheMouseCursor(e);
+
+    // don't grab loop-locators when not in loop-mode:
+    if( samplePlayerToEdit->parameters->getLoopMode() 
+      == rosic::SamplePlaybackParameters::NO_LOOP )
+    {
+      if( locatorBeingDragged == LOOP_START_LOCATOR ||
+        locatorBeingDragged == LOOP_END_LOCATOR )
+      {
+        locatorBeingDragged = NO_OBJECT;
+      }
+    }
+
+  }
+
+  // inform our listeners about the change:
+  sendChangeMessage();
+}
+
+void SamplePlayerEditorDisplay::mouseDrag(const MouseEvent &e)
+{
+  if( samplePlayerToEdit == NULL )
+    return;
+
+  // get the position of the event in components coordinates:
+  mouseX = e.getMouseDownX() + e.getDistanceFromDragStartX();
+  mouseY = e.getMouseDownY() + e.getDistanceFromDragStartY();
+
+  // get the position of the event in system coordinates (seconds for the x-axis):
+  double x = (double) mouseX;
+  double y = (double) mouseY;
+  transformFromComponentsCoordinates(x, y);
+
+  x *=  samplePlayerToEdit->parameters->getRecordingSampleRate();
+
+  // get the sample-index for the ne loop start or end:
+  //x *= playbackParametersToEdit->getRecordingSampleRate();
+  //int sampleIndex = (int) floor(x);
+  double sampleIndex = x;
+
+  // drag the locator to a new position:
+  if ( locatorBeingDragged == START_LOCATOR )
+    samplePlayerToEdit->parameters->setPlaybackStart(sampleIndex);
+  else if ( locatorBeingDragged == END_LOCATOR )
+    samplePlayerToEdit->parameters->setPlaybackEnd(sampleIndex);
+  else if ( locatorBeingDragged == LOOP_START_LOCATOR )
+    samplePlayerToEdit->setLoopStart(sampleIndex);
+  else if ( locatorBeingDragged == LOOP_END_LOCATOR )
+    samplePlayerToEdit->setLoopEnd(sampleIndex);
+
+  // trigger a repaint:
+  repaint();
+
+  // inform our listeners about the change:
+  sendChangeMessage();
+}
+
+void SamplePlayerEditorDisplay::mouseMove(const MouseEvent &e)
+{
+  switch( whatIsUnderTheMouseCursor(e) )
+  {
+  case NO_OBJECT:       
+    currentMouseCursor = MouseCursor(MouseCursor::NormalCursor);          
+    break;
+  case START_LOCATOR:    
+    currentMouseCursor = MouseCursor(MouseCursor::LeftRightResizeCursor); 
+    break;
+  case END_LOCATOR:    
+    currentMouseCursor = MouseCursor(MouseCursor::LeftRightResizeCursor); 
+    break;
+  case LOOP_START_LOCATOR:    
+    currentMouseCursor = MouseCursor(MouseCursor::LeftRightResizeCursor); 
+    break;
+  case LOOP_END_LOCATOR:    
+    currentMouseCursor = MouseCursor(MouseCursor::LeftRightResizeCursor); 
+    break;
+  }
+}
+
+void SamplePlayerEditorDisplay::mouseUp(const MouseEvent &e)
+{
+  locatorBeingDragged = -1;
+  repaint();
+  sendChangeMessage();
+}
+
+XmlElement* SamplePlayerEditorDisplay::getStateAsXml(
+  const juce::String &stateName) const
+{
+  return InteractiveCoordinateSystem::getStateAsXml(stateName);
+}
+
+bool SamplePlayerEditorDisplay::setStateFromXml(const XmlElement &xmlState)
+{
+  return InteractiveCoordinateSystem::setStateFromXml(xmlState);
+}
+
+//=================================================================================================
--- Libraries/JUCE/modules/jura_processors/generators/jura_SamplePlayer.h
+++ Libraries/JUCE/modules/jura_processors/generators/jura_SamplePlayer.h
@@ -63,5 +63,280 @@ protected:
 
 //=================================================================================================
 
+/** This class is intended to be used as a graphical editor for a waveform.
+
+-deadlock in the waveform display when destructor is called while resized has not yet returned
+-to reproduce it, create an instance inside some GUI, call SamplePlayerEditorDisplay::resized in 
+the GUIs resized method (and nothing thereafter), open the GUI in JUCEPluginHost
+-perhaps we should implement drawComponent instead of overriding paint */
+
+class SamplePlayerEditorDisplay	: virtual public WaveformDisplay, public ChangeBroadcaster
+  //, virtual public InteractiveCoordinateSystem
+{
+
+public:
+
+  //---------------------------------------------------------------------------------------------
+  // construction/destruction:
+
+  /** Constructor. */
+  SamplePlayerEditorDisplay(AudioFileBuffer *newBufferToUse = NULL);   
+
+  /** Destructor. */
+  virtual ~SamplePlayerEditorDisplay(); 
+
+  //-----------------------------------------------------------------------------------------------
+  // setup:
+
+  /*
+  virtual CoordinateSystemRangeOld getMaximumMeaningfulRange(double relativeMarginLeft = 10.0, 
+  double relativeMarginRight  = 10.0, double relativeMarginTop  = 10.0, 
+  double relativeMarginBottom = 10.0);
+  */
+  /**< Returns an Rectangle wich encloses the curve. Optionally, a margin can
+  be specified in percent. */
+
+  /** Sets the rosic::SamplePlayer object which is to be edited. */
+  virtual void setSamplePlayerToEdit(rosic::SamplePlayer *newPlayerToEdit);
+
+  /** Passes a new audiofile to be used here. Returns flase when there is some error in opening 
+  the file */
+  virtual bool setAudioFileToUse(const juce::File &newFileToUse);
+
+  /** Sets the rosic::SampleBuffer object which is to be edited. */
+  //virtual void setSampleBufferToEdit(rosic::SampleBuffer *newBufferToEdit);
+
+  /** Sets the Pointer to the rosic::SamplePlaybackParameters which hold the playback info (loop 
+  settings etc.) */
+  //virtual void setSamplePlaybackParametersToEdit(
+  //  rosic::SamplePlaybackParameters *newParametersToEdit);
+
+  /** Sets the actual waveform-data and informs about success. */
+  //virtual bool setWaveform(double** newWaveformData, int newNumSampleFrames, int newNumChannels);
+
+  /** Sets the waveform-data from an AudioSampleBuffer and informs about success. */
+  //virtual bool setWaveform(const AudioSampleBuffer& newWaveformBuffer);
+
+  /** Causes the internal peakData (inherited from WaveformDisplay) to be recalculated from the
+  waveform contained in the rosic::SampleModulator object which is being edited. */
+  virtual void updatePlot(bool resetZoomFactor = false);
+
+  /** Overrides the paint-function of the base-classes. */
+  virtual void paint(Graphics &g);
+
+  /** Overides the getStateAsXml()-method from the CoordinateSystem base-class. */
+  virtual XmlElement* getStateAsXml(
+    const juce::String& stateName = juce::String("SamplePlayerEditorDisplayState")) const;
+
+  /** Overwrites the setStateFromXml()-method from the CoordinateSystem base-class. */
+  virtual bool setStateFromXml(const XmlElement &xmlState);
+
+
+protected:
+
+  enum mousableObjects
+  {
+    NO_OBJECT = 0,
+    START_LOCATOR,
+    END_LOCATOR,
+    LOOP_START_LOCATOR,
+    LOOP_END_LOCATOR
+  };
+
+  int   locatorBeingDragged; // index of the locator which is being dragged 
+                             // (see enum above)
+
+                             // some overrides for mouse-events:
+  virtual void mouseDown(const MouseEvent &e);
+  virtual void mouseDrag(const MouseEvent &e);
+  virtual void mouseMove(const MouseEvent &e);
+  virtual void mouseUp  (const MouseEvent &e);
+
+  /** Returns an index of the object which is currently under the mouse-cursor. 
+  @see mousableObjects */
+  virtual int whatIsUnderTheMouseCursor(const MouseEvent &e);
+
+  rosic::SamplePlayer *samplePlayerToEdit;
+
+  jura::AudioFileBuffer buffer;
+  // we have a by-value member-variable here to pass its address to the inherited by-reference
+  // member (see constructor implementation)
+
+  //rosic::SampleBuffer *bufferToEdit;
+  /**< Pointer to the actual rosic::SampleBuffer object which is being edited. */
+
+  //rosic::SamplePlaybackParameters *playbackParametersToEdit;
+  /**< Pointer to the rosic::SamplePlaybackParameters which hold the playback info (loop 
+  settings etc.). */
+
+  juce_UseDebuggingNewOperator;
+};
+
+//=================================================================================================
+
+/**
+
+This is a class for a context menu that can be opened to show and edit a comprehensive set of
+parameters for a SamplesPlayer. It is used by SamplePlayerEditor by clicking on the 
+'More' button.
+
+*/
+
+class SamplePlayerEditorContextMenu : public ColourSchemeComponent,  public ChangeBroadcaster 
+  //public ButtonListener //, public RSliderListener
+{
+
+public:
+
+  //---------------------------------------------------------------------------------------------
+  // construction/destruction:
+
+  SamplePlayerEditorContextMenu(SamplePlayerAudioModule* newSamplePlayerModuleToEdit, 
+    Component* componentToAttachTo);  
+  virtual ~SamplePlayerEditorContextMenu();  
+
+  //---------------------------------------------------------------------------------------------
+  // callbacks:
+
+  //virtual void rButtonClicked(RButton *buttonThatWasClicked);
+  //virtual void rSliderValueChanged(RSlider *sliderThatHasChanged);
+  virtual void resized();
+  //virtual void updateWidgetsAccordingToState();
+
+  //---------------------------------------------------------------------------------------------
+  // widgets:
+
+  RButton    *closeButton;
+  RButton    *loopButton, *muteButton, *soloButton, *phaseRandomizeButton;
+  RSlider    *levelSlider, *levelByKeySlider, *levelByVelSlider, *panSlider, *midSideSlider,
+    *tuneSlider, *tuneByKeySlider, *tuneByVelSlider, *rootKeySlider, 
+    *startSlider, *startByVelSlider, *loopStartSlider, *loopLengthSlider, 
+    *lowpassSlider, *highpassSlider,
+    *phaseSeedSlider;
+  RTextField *ampHeadline, *tuningHeadline, *timeHeadline, *filterHeadline, *miscHeadline;
+
+  //=============================================================================================
+  juce_UseDebuggingNewOperator;
+
+protected:
+
+  /** Pointer to the actual SamplePlayerAudioModule object which is being edited. */
+  SamplePlayerAudioModule* samplePlayerModuleToEdit;
+
+};
+
+
+
+//===============================================================================================
+
+/** This class implements an editor for the SamplePlayerAudio with waveform display.
+
+\todo: 
+-move the File-management stuff into the audio module - see LFO
+-we must also maintain a state -> don't derive from AudioFileManager, embedd one instead  */
+
+class SamplePlayerModuleEditor : virtual public AudioModuleEditor,  public ChangeBroadcaster, 
+  public AudioFileManager, public RSliderListener
+{
+
+public:
+
+  //---------------------------------------------------------------------------------------------
+  // construction/destruction:
+
+  /** Constructor. */
+  SamplePlayerModuleEditor(CriticalSection *newPlugInLock, 
+    SamplePlayerAudioModule* newSamplePlayerModuleToEdit);
+
+  /** Destructor. */
+  virtual ~SamplePlayerModuleEditor();
+
+  //---------------------------------------------------------------------------------------------
+  // setup:
+
+  /** Passes a pointer the the actual rosic::SampleBufferPlayer object which is to be edited. 
+  Make sure to call this function again with a NULL-pointer when the object get deleted for some 
+  reason. */
+  //virtual void setSamplePlayerToEdit(rosic::SamplePlayer* newSamplePlayerToEdit);
+
+  /** Sets the juce::Label in which the descriptions for the widgets will appear. */
+  //virtual void setDescriptionField(RLabel* newDescriptionField);
+
+  /** Load a new sample. */
+  //virtual bool setSampleFromFile(const File &fileToLoadFrom);
+
+  //---------------------------------------------------------------------------------------------
+  // callbacks:
+
+  /** Implements the purely virtual rButtonClicked()-method of the ButtonListener base-class. */
+  virtual void rButtonClicked(RButton *buttonThatWasClicked);
+
+  /** Implements the purely virtual changeListenerCallback()-method of the ChangeListener 
+  base-class. */
+  virtual void changeListenerCallback(ChangeBroadcaster *objectThatHasChanged);
+
+  /** Implements the purely virtual rSliderValueChanged()-method of the RSliderListener 
+  base-class. */
+  virtual void rSliderValueChanged(RSlider *sliderThatHasChanged);
+
+  /** Overrides paint() */
+  virtual void paint(Graphics &g);
+
+  /** Overrides resized() */
+  virtual void resized();
+
+  /** Overrides the purely virtual AudioFileManager::setAudioData. */
+  virtual bool setAudioData(AudioSampleBuffer* newBuffer, const juce::File& underlyingFile, 
+    bool markAsClean);
+
+  //---------------------------------------------------------------------------------------------
+  // others:
+
+  /** Updates the sliders, buttons, etc. according to the state of the rosic::StereoOscillator 
+  object which is being edited. */
+  virtual void updateWidgetsAccordingToState(bool updateSampleDisplayAlso);
+
+  /** Updates the sliders, buttons, etc. according to the state of the rosic::StereoOscillator 
+  object which is being edited. */
+  virtual void updateWidgetsAccordingToState();
+
+  /** Opens the dialog for loading a new sample. */
+  //virtual bool openSampleLoadingDialog();
+
+  //---------------------------------------------------------------------------------------------
+  // public data members:
+
+  SamplePlayerEditorDisplay     *sampleDisplay;
+  CoordinateSystemZoomer        *sampleDisplayZoomer;
+  SamplePlayerEditorContextMenu *contextMenu;
+
+  RTextField *fileLabel, *sampleFileNameLabel, *formatLabel, *formatInfoLabel; 
+
+  RButton *sampleLoadButton, *samplePlusButton, *sampleMinusButton, *loopButton, *muteButton, 
+    *soloButton, *phaseRandomizeButton, *moreButton, *fromLoopButton, *loopSnapButton, 
+    *loopLengthLockButton, *autoNumCyclesButton;
+
+  RSlider *levelSlider, *tuneSlider, *lowpassSlider, *highpassSlider, *rootKeySlider, 
+    *rootDetuneSlider, *startSlider, *startByVelSlider, *endSlider, *loopStartSlider, 
+    *loopEndSlider, *loopLengthSlider, *loopCrossfadeTimeSlider, *loopCrossfadeShapeSlider, 
+    *loopNumCyclesSlider;
+
+protected:
+
+  //virtual void updateLoopWidgets
+
+  /** Updates the waveform display plot. */   
+  //virtual void updatePlot();
+
+  /** Pointer to the actual SampleBufferPlayer object which is being edited. */
+  //rosic::SamplePlayer* samplePlayerToEdit;
+  SamplePlayerAudioModule* samplePlayerModuleToEdit;
+
+  // some rectangles to define functional groups:
+  juce::Rectangle<int> fileRectangle, loopRectangle;
+
+  juce_UseDebuggingNewOperator;
+};
+
 
 #endif 
