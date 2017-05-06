fdf4f62accfbd730b4259aa7070e7378b5950eb9
Robin Schmidt <robin@rs-met.com>
2017-05-06 18:40:10 +0200

    added editor to VectorMixer

--- Libraries/JUCE/modules/jura_processors/generators/jura_VectorMixer.cpp
+++ Libraries/JUCE/modules/jura_processors/generators/jura_VectorMixer.cpp
@@ -85,3 +85,389 @@ void VectorMixerAudioModule::initializeAutomatableParameters()
 }
 
 //=================================================================================================
+
+
+VectorMixerPad::VectorMixerPad(rosic::VectorMixer* newVectorMixerToEdit, const juce::String& name) 
+  : CoordinateSystemOld(name)
+{
+  setDescription("Drag around the dot to adjust the mix between the 4 signals");
+
+  // indicate that this ParameterObserver is a GUI element:
+  ParameterObserver::isGuiElement = true;
+
+  // assign the pointer to the VectorMixer to be edited:
+  vectorMixerToEdit = newVectorMixerToEdit;
+
+  // set up the plot range:
+  setMaximumRange(-1.1, 1.1, -1.1, 1.1);
+  setCurrentRange(-1.1, 1.1, -1.1, 1.1);
+  setHorizontalCoarseGrid(1.0,  true);
+  //setHorizontalFineGrid(  0.25, true);
+  setVerticalCoarseGrid(  1.0,  true);
+  //setVerticalFineGrid(    0.25, true);
+
+  // use a normal mouse cursor:
+  currentMouseCursor = MouseCursor(MouseCursor::NormalCursor);
+  setMouseCursor(currentMouseCursor);
+
+  // choose some nice radius for the handle:
+  dotRadius = 5.f;
+  dotColour = Colours::blue;
+
+  // initialize the two pointers to Parameter objects with NULL:
+  xParameter  = NULL;
+  yParameter  = NULL;
+
+  // activate automation for this ParameterObserver:
+  ParameterObserver::localAutomationSwitch = true;
+}
+
+VectorMixerPad::~VectorMixerPad()
+{
+  // remove ourselves as listeners from the Parameter objects, such that they do 
+  // not try to notify a nonexistent listener:
+  ParameterObserver::localAutomationSwitch = false;
+  if( xParameter != NULL )
+    xParameter->deRegisterParameterObserver(this);
+  if( yParameter != NULL )
+    yParameter->deRegisterParameterObserver(this);
+}
+
+//-------------------------------------------------------------------------------------------------
+// parameter-settings:
+
+void VectorMixerPad::setVectorMixerToEdit(rosic::VectorMixer* newVectorMixerToEdit)
+{
+  vectorMixerToEdit = newVectorMixerToEdit;
+}
+
+void VectorMixerPad::assignParameterX(Parameter *parameterToAssign)
+{
+  xParameter = parameterToAssign;
+  if( xParameter != NULL )
+    xParameter->registerParameterObserver(this);
+}
+
+void VectorMixerPad::assignParameterY(Parameter *parameterToAssign)
+{
+  yParameter = parameterToAssign;
+  if( yParameter != NULL )
+    yParameter->registerParameterObserver(this);
+}
+
+void VectorMixerPad::unAssignParameterX()
+{
+  if( xParameter != NULL )
+    xParameter->deRegisterParameterObserver(this);
+  xParameter = NULL;
+}
+
+void VectorMixerPad::unAssignParameterY()
+{
+  if( yParameter != NULL )
+    yParameter->deRegisterParameterObserver(this);
+  yParameter = NULL;
+}
+
+void VectorMixerPad::parameterChanged(Parameter* parameterThatHasChanged)
+{
+  // send out a change-message: 
+  sendChangeMessage();
+  //updatePlot();
+}
+
+void VectorMixerPad::updateWidgetFromAssignedParameter(bool sendMessage)
+{
+  updateBackgroundImage();
+  if( sendMessage == true )
+    sendChangeMessage();
+}
+
+//-------------------------------------------------------------------------------------------------
+// callbacks:
+
+void VectorMixerPad::changeListenerCallback(ChangeBroadcaster *objectThatHasChanged)
+{
+  // temporarily switch the wantsAutomationNotification flag from the ParameterObserver base 
+  // class off to avoid circular notifications and updates:
+  localAutomationSwitch = false;
+
+  // call the method which updates the widget:
+  updateBackgroundImage();
+  //updateWidgetFromAssignedParameter(false);
+
+
+  // switch the wantsAutomationNotification flag on again:  
+  localAutomationSwitch = true;
+}
+
+void VectorMixerPad::mouseDown(const MouseEvent &e)
+{
+  if( vectorMixerToEdit == NULL )
+    return;
+
+  // preliminray: do not open the MIDI-learn menu on right-button - show the image export menu 
+  // instead (inherited behaviour from CoordinateSytem):
+  if( e.mods.isRightButtonDown() )
+    CoordinateSystemOld::mouseDown(e);
+  else
+  {
+    // get the position of the event in components coordinates
+    double x = e.getMouseDownX();
+    double y = e.getMouseDownY();
+
+    setupVectorMixerAccordingToMousePosition(x, y);
+
+    // send out a change-message:
+    sendChangeMessage();
+  }
+
+  /*
+  if( e.mods.isRightButtonDown() && xParameter != NULL && yParameter != NULL )
+  {
+  // prepare some strings for the popup menu:
+  int freqCC = xParameter->getAssignedMidiController();
+  juce::String freqjuce::String;
+  if( freqCC > -1 )
+  freqjuce::String = juce::String(T("(currently CC#")) + juce::String(freqCC) + juce::String(T(")"));
+  else
+  freqjuce::String = juce::String(T("(currently none)")); 
+  juce::String minFreqjuce::String = hertzToStringWithUnitTotal5(xParameter->getLowerAutomationLimit());
+  juce::String maxFreqjuce::String = hertzToStringWithUnitTotal5(xParameter->getUpperAutomationLimit());
+
+  int resoCC = yParameter->getAssignedMidiController();
+  juce::String resojuce::String;
+  if( resoCC > -1 )
+  resojuce::String = juce::String(T("(currently CC#")) + juce::String(resoCC) + juce::String(T(")"));
+  else
+  resojuce::String = juce::String(T("(currently none)")); 
+
+  // ToDo: different cases - y can be reso, q or gain
+  juce::String minResojuce::String = percentToStringWithUnit1(yParameter->getLowerAutomationLimit());
+  juce::String maxResojuce::String = percentToStringWithUnit1(yParameter->getUpperAutomationLimit());
+
+  // create a context menu to allow for MIDI learn and setting up min and max automation values:
+  PopupMenu menu;
+  menu.addItem(1, juce::String(T("MIDI learn frequency ") + freqjuce::String)  );
+  menu.addItem(2, juce::String(T("MIDI learn resonance ") + resojuce::String)  );
+  menu.addItem(3, juce::String(T("use current values as lower limits"))  );
+  menu.addItem(4, juce::String(T("use current values as upper limits"))  );
+  menu.addItem(5, juce::String(T("revert to defaults"))                  );
+
+  const int result = menu.show();
+
+  // retrieve current characteristic frequency and resonance:
+  double freq = filterToEdit->getFrequencyNominal(); // frequency
+  double reso = filterToEdit->getResonance();        // resonance
+
+  if (result == 0)
+  {
+  // user dismissed the menu without picking anything - do nothing
+  }
+  else if (result == 1)
+  {
+  // user picked the frequency learn item:
+  xParameter->switchIntoMidiLearnMode();
+  }
+  else if (result == 2)
+  {
+  // user picked the resonance learn item:
+  yParameter->switchIntoMidiLearnMode();
+  }
+  else if (result == 3)
+  {
+  // user picked the lower-limit item:
+  xParameter->setLowerAutomationLimit(freq);
+  yParameter->setLowerAutomationLimit(reso);
+  }
+  else if (result == 4)
+  {
+  // user picked the upper-limit item:
+  xParameter->setUpperAutomationLimit(freq);
+  yParameter->setUpperAutomationLimit(reso);
+  }
+  else if (result == 5)
+  {
+  // user picked the revert to defaults item:
+  xParameter->revertToDefaults();
+  yParameter->revertToDefaults();
+  }
+  } // end of  if( e.mods.isRightButtonDown() )
+  */
+}
+
+void VectorMixerPad::mouseDrag(const juce::MouseEvent &e)
+{
+  if( vectorMixerToEdit == NULL )
+    return;
+
+  /*
+  if( e.mods.isRightButtonDown() && xParameter != NULL && yParameter != NULL )
+  {
+  // ignore mouse drags whne the right button is down and we have assigned automatable 
+  // parameters because in that case, the right click is used for opening the MIDI-learn popup
+  }
+  else...
+  */
+
+  // get the position of the event in components coordinates
+  double x = e.getMouseDownX() + e.getDistanceFromDragStartX();
+  double y = e.getMouseDownY() + e.getDistanceFromDragStartY();
+
+  setupVectorMixerAccordingToMousePosition(x, y);
+
+  // send out a change-message:
+  sendChangeMessage();
+}
+
+void VectorMixerPad::setupVectorMixerAccordingToMousePosition(double mouseX, double mouseY)
+{
+  if( vectorMixerToEdit == NULL )
+    return;
+
+  // get the position of the event in components coordinates
+  double x = mouseX;
+  double y = mouseY;
+
+  // convert them into the vectorMixer's coordinates:
+  transformFromComponentsCoordinates(x, y);
+
+  // set up the VectorMixer and raise automation events to update other widgets that represent the
+  // parameters:
+  vectorMixerToEdit->setXY(x, y);
+  if( xParameter != NULL )
+    xParameter->setValue(x, true, true);
+  if( yParameter != NULL )
+    yParameter->setValue(y, true, true);
+}
+
+//-------------------------------------------------------------------------------------------------
+// drawing:
+
+void VectorMixerPad::resized()
+{
+  CoordinateSystemOld::resized();
+  updateBackgroundImage();
+}
+
+void VectorMixerPad::drawCoordinateSystem(Graphics &g, juce::Image* targetImage, XmlElement *targetSVG)
+{
+  CoordinateSystemOld::drawCoordinateSystem(g, targetImage, targetSVG);
+
+  if( vectorMixerToEdit == NULL )
+    return;
+
+  // draw the dot-handle and a crosshair:
+  double x = vectorMixerToEdit->getX();
+  double y = vectorMixerToEdit->getY();
+
+  if( targetImage == NULL )
+    transformToComponentsCoordinates(x, y);
+  else
+    transformToImageCoordinates(x, y, targetImage);
+
+  g.setColour(dotColour);
+  g.fillEllipse((float) (x-dotRadius), (float) (y-dotRadius), 
+    (float) (2*dotRadius), (float) (2*dotRadius) );
+  g.setColour(dotColour.withAlpha(0.4f));
+  float w = (float) getPlotWidth(targetImage);
+  float h = (float) getPlotHeight(targetImage);
+  g.drawLine(       0,(float)y,        w, (float)y, 1.f);  // horizontal
+  g.drawLine((float)x,       0, (float)x,        h, 1.f);  // vertical
+}
+
+//=================================================================================================
+
+// construction/destruction:
+
+VectorMixerModuleEditor::VectorMixerModuleEditor(CriticalSection *newPlugInLock, 
+  VectorMixerAudioModule* newVectorMixerAudioModule) 
+  : AudioModuleEditor(newVectorMixerAudioModule) //, VectorMixerPad(NULL)
+{
+  // assign the pointer to the rosic::VectorMixer object to be used as audio engine:
+  jassert( newVectorMixerAudioModule != NULL ); // you must pass a valid module here
+  //VectorMixerPad::setVectorMixerToEdit(newVectorMixerAudioModule->wrappedVectorMixer);
+
+  vectorPad = new VectorMixerPad(newVectorMixerAudioModule->wrappedVectorMixer);
+  vectorPad->setDescriptionField(infoField);
+  vectorPad->addChangeListener(this);
+  addPlot( vectorPad );
+
+  // set up the widgets:
+  updateWidgetsAccordingToState();
+}
+
+//-------------------------------------------------------------------------------------------------
+// overrides:
+
+/*
+void VectorMixerModuleEditor::updateWidgetsAccordingToState()
+{
+VectorMixerPad::updateWidgetsAccordingToState();
+}
+*/
+
+/*
+void VectorMixerModuleEditor::mouseDown(const MouseEvent &e)
+{
+VectorMixerPad::mouseDown(e);
+}
+
+void VectorMixerModuleEditor::mouseDrag(const MouseEvent &e)
+{
+VectorMixerPad::mouseDrag(e);
+}
+
+void VectorMixerModuleEditor::mouseMove(const MouseEvent &e)
+{
+VectorMixerPad::mouseMove(e);
+}
+
+void VectorMixerModuleEditor::mouseEnter(const MouseEvent &e)
+{
+VectorMixerPad::mouseEnter(e);
+}
+
+void VectorMixerModuleEditor::mouseExit(const MouseEvent &e)
+{
+VectorMixerPad::mouseExit(e);
+}
+
+void VectorMixerModuleEditor::mouseUp(const MouseEvent &e)
+{
+VectorMixerPad::mouseUp(e);
+}
+
+void VectorMixerModuleEditor::mouseDoubleClick(const MouseEvent &e)
+{
+VectorMixerPad::mouseDoubleClick(e);
+}
+
+void VectorMixerModuleEditor::mouseWheelMove(const MouseEvent &e, 
+float wheelIncrementX, float wheelIncrementY)
+{
+VectorMixerPad::mouseWheelMove(e, wheelIncrementX, wheelIncrementY);
+}
+
+void VectorMixerModuleEditor::paint(Graphics &g)
+{
+VectorMixerPad::paint(g);
+}
+*/
+void VectorMixerModuleEditor::resized()
+{
+  //vectorPad->setBounds(getBounds());
+  vectorPad->setBounds(0, 0, getWidth(), getHeight());
+}
+
+/*
+XmlElement* VectorMixerModuleEditor::getStateAsXml(const juce::String& stateName) const
+{
+return AudioModuleEditor::getStateAsXml(stateName);
+}
+
+bool VectorMixerModuleEditor::setStateFromXml(const XmlElement& xmlState, const juce::String& name)
+{
+return AudioModuleEditor::setStateFromXml(xmlState, name);
+}
+*/
--- Libraries/JUCE/modules/jura_processors/generators/jura_VectorMixer.h
+++ Libraries/JUCE/modules/jura_processors/generators/jura_VectorMixer.h
@@ -48,4 +48,147 @@ protected:
   juce_UseDebuggingNewOperator;
 };
 
+//=================================================================================================
+
+/**
+
+This class implements a user interface XY-pad for a rosic::VectorMixer object.
+
+\todo turn this into a general purpose widget with callback and obesrver
+callback: xyPadChanged(double newX, double newY, bool xWasChanged, bool yWasChanged);
+
+*/
+
+class VectorMixerPad	: virtual public CoordinateSystemOld, public ParameterObserver, 
+  public ChangeBroadcaster
+{
+
+public:
+
+  //---------------------------------------------------------------------------------------------
+  // construction/destruction:
+
+  /** Constructor. */
+  VectorMixerPad(rosic::VectorMixer* newVectorMixerToEdit, 
+    const juce::String& name = juce::String("XYPad")  );   
+
+  /** Destructor. */
+  virtual ~VectorMixerPad(); 
+
+  //---------------------------------------------------------------------------------------------
+  // setup:
+
+  /** Passes a pointer the the actual rosic::MoogyFilter object which is to be edited. Make 
+  sure to call this function again with a NULL-pointer when the object get deleted for some 
+  reason. */
+  virtual void setVectorMixerToEdit(rosic::VectorMixer* newVectorMixerToEdit);
+
+  /** Assigns a rojue::Parameter object to the horizontal axis for observation and 
+  manipulation. */
+  virtual void assignParameterX(Parameter* parameterToAssign);
+
+  /** Assigns a rojue::Parameter object to the vertical axis for observation and 
+  manipulation. */
+  virtual void assignParameterY(Parameter* parameterToAssign);
+
+  /** Un-Assigns a previously rojue::Parameter object for the horizontal axis. */
+  virtual void unAssignParameterX();
+
+  /** Un-Assigns a previously rojue::Parameter object for the vertical axis. */
+  virtual void unAssignParameterY();
+
+  /** This method is called when one of the assigned rojue::Parameters has been changed
+  - we override it here in the subclass to do the actual GUI update. */
+  virtual void updateWidgetFromAssignedParameter(bool sendMessage = false);
+
+  //-----------------------------------------------------------------------------------------------
+  // callbacks:
+
+  virtual void parameterChanged(Parameter* parameterThatHasChanged);
+  virtual void parameterIsGoingToBeDeleted(Parameter* parameterThatWillBeDeleted) {}
+
+  /** Overrides the changeListetnerCcallback in order to receive messages which this object sends
+  to itself. */
+  virtual void changeListenerCallback(ChangeBroadcaster *objectThatHasChanged);
+
+  /** Overrides mouseDown for adjusting the frequency and resonance and lets a context menu pop 
+  up when the right button is clicked for MIDI-learn functionality. */
+  virtual void mouseDown(const MouseEvent& e);
+
+  /** Overrides mouseDrag for adjusting the frequency and resonance. */
+  virtual void mouseDrag(const MouseEvent& e);
+
+  /** Overrides the resized-method. */
+  virtual void resized();
+
+  /** Updates the vector plot. */
+  //virtual void updatePlot();
+
+  /** Overrides the inherited method from the CoordinateSystem base-class. */
+  virtual void drawCoordinateSystem(Graphics &g, juce::Image* targetImage = NULL, 
+    XmlElement* targetSVG = NULL);
+
+protected:
+
+  /** Does the setup of the VectorMixer according to some new mouse position. */
+  virtual void setupVectorMixerAccordingToMousePosition(double mouseX, double mouseY);
+
+  /** Pointer to the actual rosic::VectorMixer object which is being edited. */
+  rosic::VectorMixer* vectorMixerToEdit;
+
+  /** Radius of the dot-handle to be drawn. */
+  float dotRadius;
+
+  Colour dotColour;
+
+  // the parameters which will cause re-plotting and therefore must be listened to:
+  Parameter* xParameter;
+  Parameter* yParameter;
+
+  juce_UseDebuggingNewOperator;
+};
+
+//=================================================================================================
+
+//class VectorMixerModuleEditor : virtual public AudioModuleEditor, virtual public VectorMixerPad
+class VectorMixerModuleEditor : public AudioModuleEditor
+{
+
+public:
+
+  //---------------------------------------------------------------------------------------------
+  // construction/destruction:
+
+  /** Constructor. You must pass a valid (non NULL) pointer to an VectorMixerAudioModule 
+  object which will be accessed by this editor. */
+  VectorMixerModuleEditor(CriticalSection *newPlugInLock, 
+    VectorMixerAudioModule* newVectorMixerAudioModule);
+
+  //---------------------------------------------------------------------------------------------
+  // overrides to avoid ambigous access errors and dominance warnings:
+  /*
+  virtual void mouseDown(       const MouseEvent &e);
+  virtual void mouseDrag(       const MouseEvent &e);
+  virtual void mouseMove(       const MouseEvent &e);
+  virtual void mouseEnter(      const MouseEvent &e);
+  virtual void mouseExit(       const MouseEvent &e);
+  virtual void mouseUp(         const MouseEvent &e);
+  virtual void mouseDoubleClick(const MouseEvent &e);
+  virtual void mouseWheelMove(  const MouseEvent &e, 
+  float wheelIncrementX, float wheelIncrementY);
+  virtual void paint(Graphics &g);
+  */
+  virtual void resized();
+
+  //virtual void updateWidgetsAccordingToState();
+  //virtual void setStateFromXml(const XmlElement& xmlState, const juce::String& name);
+  //virtual XmlElement* getStateAsXml(const juce::String& stateName = juce::String(T("VectorPadState"))) const;
+
+protected:
+
+  VectorMixerPad *vectorPad;
+
+  juce_UseDebuggingNewOperator;
+};
+
 #endif 
