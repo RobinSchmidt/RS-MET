e01c90b0b1608e25536e447262f3dbd0f9f0f579
Robin Schmidt <robin@rs-met.com>
2017-05-08 13:17:11 +0200

    added some includes to romos (still commented)

--- Libraries/JUCE/modules/romos/romos.cpp
+++ Libraries/JUCE/modules/romos/romos.cpp
@@ -9,21 +9,36 @@ header files that the compiler may be using.
 
 #include "romos.h"
 
-namespace romos
-{
+//namespace romos { // it'S still all wrapped into a namespace inside the included files
 
 #include "Framework/romos_ModuleTypeRegistry.cpp"
+#include "Framework/romos_ProcessingStatus.cpp"
 
-
+//// if everything else is commented, up to here, we get no linker errors when building
+//
 //#include "Framework/romos_AudioConnection.cpp"
-
+//#include "Framework/romos_VoiceAllocator.cpp"
+//#include "Framework/romos_WorkArea.cpp"
 //#include "Framework/romos_Module.cpp"
-
-//#include "Framework/romos_ModuleAtomic.cpp"
-
-//#include "Framework/romos_ModuleFactory.cpp"
 //#include "Framework/romos_ModuleContainer.cpp" 
+//#include "Framework/romos_ModuleAtomic.cpp"
+//
+//#include "Modules/romos_ArithmeticModules.cpp"
+//#include "Modules/romos_DelayModules.cpp"
+//#include "Modules/romos_FilterModules.cpp"
+//#include "Modules/romos_FunctionModules.cpp"
+//#include "Modules/romos_InfrastructuralModules.cpp"
+//#include "Modules/romos_ModulationModules.cpp"
+//#include "Modules/romos_SoundGeneratorModules.cpp"
+//
+//
+//
+//#include "TestSuite/romos_TestModuleBuilder.cpp"  // seems to need all the atomic modules
+//#include "Framework/romos_ModuleFactory.cpp"      // need TestModuleBuilder
+
+
+
 //#include "Framework/romos_TopLevelModule.cpp"
 
 
-}
+//}
