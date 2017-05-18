551fdb1b24186355e8631ee0492297394508cef8
Robin Schmidt <robin@rs-met.com>
2017-05-18 18:51:43 +0200

    fixed linker errors for rosic testbed

--- Tests/TestsRosicAndRapt/Source/Main.cpp
+++ Tests/TestsRosicAndRapt/Source/Main.cpp
@@ -11,7 +11,7 @@ int main (int argc, char* argv[])
   //testRosicFile();
   //testRosicEffects();
   //testRosicGenerators();
-  //testRosicFilter();
+  testRosicFilter();
   //testRosicNumerical();
   //testRosicMath();
   //testRosicNonRealTime();
