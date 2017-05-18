d26060d6f7614e99ee02756825ac9bf29d104170
Robin Schmidt <robin@rs-met.com>
2017-05-18 21:44:08 +0200

    centralized more rosic header includes

--- Libraries/JUCE/modules/rosic/rosic.h
+++ Libraries/JUCE/modules/rosic/rosic.h
@@ -23,59 +23,72 @@
 
 *******************************************************************************/
 
-
-
-
-/** ToDo: re-order the includes in the order in whcih they depend on each other */
-
 #ifndef ROSIC_H_INCLUDED
 #define ROSIC_H_INCLUDED
 
-#include <malloc.h>  // for alloca - try to get rid..
-//#include <math.h>
+/** ToDo: re-order the includes in the order in which they depend on each other */
 
 //-------------------------------------------------------------------------------------------------
 // new, centralized header includes (when finsihed, delete old ones):
 
-//#include "basics/GlobalDefinitions.h"
-//#include "basics/GlobalFunctions.h"
-//#include "basics/rosic_Constants.h"
-//#include "basics/rosic_ChannelMatrix2x2.h"
-//#include "basics/rosic_HelperFunctions.h"
-//#include "basics/rosic_NumberManipulations.h"
-//#include "basics/rosic_FunctionTemplates.h"
-//
-//// we need to intersperse some includes from other directories before we can finish the includes 
-//// from basics (todo: fix the dependency structure/layering):
-//#include "infrastructure/rosic_MutexLock.h"
-//#include "math/rosic_CephesDeclarations.h"
-//#include "math/rosic_RealFunctionEvaluationAlgorithms.h"
-//#include "math/rosic_IntegerFunctions.h"
-//#include "math/rosic_ElementaryFunctionsReal.h"
-//#include "math/rosic_SpecialFunctionsReal.h"
-//#include "math/rosic_Complex.h"
-//#include "math/rosic_ComplexFunctions.h"
-//#include "scripting/rosic_ExpressionEvaluatorFunctions.h"
-//#include "scripting/rosic_ExpressionEvaluatorComplexFunctions.h"
-//#include "scripting/rosic_ExpressionEvaluator.h"
-//
-//// ..now we can finish the includes from "basics":
-//#include "basics/rosic_Interpolator.h"                // needs ElementaryFunctionsReal
-//#include "basics/rosic_SampleBuffer.h"                // needs MutexLock
-//#include "basics/rosic_SamplePlaybackParameters.h"    // needs ElementaryFunctionsReal
-//#include "basics/rosic_TabulatedFunction.h"           // needs ExpressionEvaluator, Mutexlock
-//#include "basics/rosic_WarpedAllpassInterpolator.h"   // needs ElementaryFunctionsReal
-//#include "basics/rosic_WindowDesigner.h"              // needs SpecialFunctionsReal
+#include <malloc.h>  // for alloca - try to get rid..
+//#include <math.h>
 
+// basics:
+#include "basics/GlobalDefinitions.h"
+#include "basics/GlobalFunctions.h"
+#include "basics/rosic_Constants.h"
+#include "basics/rosic_ChannelMatrix2x2.h"
+#include "basics/rosic_HelperFunctions.h"
+#include "basics/rosic_NumberManipulations.h"
+#include "basics/rosic_FunctionTemplates.h"
+// we need to intersperse some includes from other directories before we can finish the includes 
+// from basics (todo: fix the dependency structure/layering):
+#include "infrastructure/rosic_MutexLock.h"
+#include "math/rosic_CephesDeclarations.h"
+#include "math/rosic_RealFunctionEvaluationAlgorithms.h"
+#include "math/rosic_IntegerFunctions.h"
+#include "math/rosic_ElementaryFunctionsReal.h"
+#include "math/rosic_SpecialFunctionsReal.h"
+#include "math/rosic_Complex.h"
+#include "math/rosic_ComplexFunctions.h"
+#include "scripting/rosic_ExpressionEvaluatorFunctions.h"
+#include "scripting/rosic_ExpressionEvaluatorComplexFunctions.h"
+#include "scripting/rosic_ExpressionEvaluator.h"
+// ..now we can finish the includes from "basics":
+#include "basics/rosic_Interpolator.h"                // needs ElementaryFunctionsReal
+#include "basics/rosic_SampleBuffer.h"                // needs MutexLock
+#include "basics/rosic_SamplePlaybackParameters.h"    // needs ElementaryFunctionsReal
+#include "basics/rosic_TabulatedFunction.h"           // needs ExpressionEvaluator, Mutexlock
+#include "basics/rosic_WarpedAllpassInterpolator.h"   // needs ElementaryFunctionsReal
+#include "basics/rosic_WindowDesigner.h"              // needs SpecialFunctionsReal
 
+// datastructures:
+#include "datastructures/rosic_Array.h"
+#include "datastructures/rosic_String.h"
+#include "datastructures/rosic_KeyValueMap.h"
+#include "datastructures/rosic_ExtensionsForSTL.h"
+
+// math:
+#include "math/rosic_Interpolation.h"
+#include "math/rosic_LinearAlgebra.h"
+#include "math/rosic_Matrix.h"
+#include "math/rosic_Vector.h"
+#include "math/rosic_MatrixVectorFunctions.h"
+#include "math/rosic_PolynomialAlgorithms.h"          // needs Array
+#include "math/rosic_PrimeNumbers.h"
+//#include "math/rosic_PrimeArray.h"  
+#include "math/rosic_Transformations.h"
+
+// good, until here
 
 
 //-------------------------------------------------------------------------------------------------
 // old, de-centralized header includes:
 
 #include "analysis/rosic_Analysis.h"
-#include "basics/rosic_Basics.h"
-#include "datastructures/rosic_DataStructures.h"
+//#include "basics/rosic_Basics.h"
+//#include "datastructures/rosic_DataStructures.h"
 #include "delaylines/rosic_DelayLines.h"
 #include "dynamics/rosic_Dynamics.h"
 #include "effects/rosic_Effects.h"
@@ -83,7 +96,7 @@
 #include "generators/rosic_Generators.h"
 #include "infrastructure/rosic_Infrastructure.h"
 #include "instruments/rosic_Instruments.h"
-#include "math/rosic_Math.h"
+//#include "math/rosic_Math.h"
 #include "modulators/rosic_Modulators.h"
 #include "neural/rosic_Neural.h"
 #include "numerical/rosic_Numerical.h"
