024b5f85e8a955233aae2d7e81dfa67228185078
Robin Schmidt <robin@rs-met.com>
2017-11-02 22:09:49 +0100

    added some stuff to jura_SmoothableParameter.h

--- Libraries/JUCE/modules/jura_framework/control/jura_SmoothableParameter.h
+++ Libraries/JUCE/modules/jura_framework/control/jura_SmoothableParameter.h
@@ -1,22 +1,56 @@
 #ifndef jura_SmoothableParameter_h
 #define jura_SmoothableParameter_h
 
+//=================================================================================================
 
-
-class JUCE_API SmoothingManager
+class JUCE_API rsSmoother
 {
 
+public:
+
+  void update()
+  {
+    //currentValue = smoothingFilter.getSample(targetValue);
+
+    // \todo:
+    // -call a value-change callback, 
+    // -maybe return a bool that indicates if target has been reached which can be 
+    //  used by rsSmoothingManager to deactivate the smoother
+  }
+
+protected:
+
+
+  double currentValue, targetValue;
+
+  //RAPT::rsSmoothingFilter<double, double> smoothingFilter;
+    // ahh..damn...we don't have access to rapt here...smoothing stuff needs to be moved to
+    // jura_processors
+
+};
+
+
+//=================================================================================================
+
+class JUCE_API rsSmoothingManager
+{
+
+
+protected:
+
+  std::vector<rsSmoother*> smootherPool;
+  std::vector<rsSmoother*> usedSmoothers;
 
 };
 
 //=================================================================================================
 
-class JUCE_API SmoothableParameter : public Parameter
+class JUCE_API rsSmoothableParameter : public Parameter
 {
 
 public:
 
-  virtual ~SmoothableParameter(){};
+  virtual ~rsSmoothableParameter(){};
 
 
   virtual void setValue(double newValue, bool sendNotification, bool callCallbacks) override;
@@ -25,7 +59,7 @@ public:
 protected:
 
 
-  SmoothingManager* smoothingManager = nullptr;
+  rsSmoothingManager* smoothingManager = nullptr;
 
 };
 
