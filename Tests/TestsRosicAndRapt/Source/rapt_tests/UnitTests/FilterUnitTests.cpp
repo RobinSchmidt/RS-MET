a62dc7e70a6a16e549cfbc3faa39941a26fda729
Robin Schmidt <robin@rs-met.com>
2017-11-23 16:34:52 +0100

    added code to prototypeDesignUnitTest

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/FilterUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/FilterUnitTests.cpp
@@ -1,29 +1,45 @@
 #include "FilterUnitTests.h"
 
+// maybe move somewhere else for sharing
+bool isCloseTo(complex<float> x, complex<float> y, float tol)
+{
+  return abs(x-y) <= tol;
+}
+
 bool prototypeDesignUnitTest()
 {
-	bool r = true; // test result
-
   // shorthands:
   typedef rsPrototypeDesignerF PD;
   typedef complex<float> CF;
   float inf = std::numeric_limits<float>::infinity();
 
-  CF p[5], z[5]; // arrays for retrieved poles
-  PD pd;         // prototype designer object
+	bool r = true;   // test result
+  float tol = 0.f; // zero tolerance - float comparisons are currently exact
+  CF p[3], z[3];   // arrays for retrieved poles
+  PD pd;           // prototype designer object
 
-  // 5th order Papoulis lowpass:
+  // Papoulis filters:
   pd.setApproximationMethod(PD::PAPOULIS);
   pd.setPrototypeMode(PD::LOWPASS_PROTOTYPE);
-  pd.setOrder(5);
-  pd.getPolesAndZeros(&p[0], &z[0]);                 // non-redundant upper halfplane poles/zeros
-  r &= p[0] == CF(-0.153586745f, 0.968145967f);
-  r &= p[1] == CF(-0.388139844f, 0.588632464f);
-  r &= p[2] == CF(-0.468089849f, 0.f);
+  pd.setOrder(5);                                               // 5th order lowpass
+  pd.getPolesAndZeros(&p[0], &z[0]);              
+  r &= isCloseTo(p[0], CF(-0.153586745f, 0.968145967f), tol);
+  r &= isCloseTo(p[1], CF(-0.388139844f, 0.588632464f), tol);
+  r &= isCloseTo(p[2], CF(-0.468089849f, 0.f),          tol);
   r &= z[0] == CF(inf, 0.f);
   r &= z[1] == CF(inf, 0.f);
   r &= z[2] == CF(inf, 0.f);
   r &= pd.getNumFinitePoles() == 5;
+  r &= pd.getNumFiniteZeros() == 0;
+  pd.setOrder(6);                                               // 6th order lowpass
+  pd.getPolesAndZeros(&p[0], &z[0]);
+  r &= isCloseTo(p[0], CF(-0.115192428f, 0.977922320f), tol);
+  r &= isCloseTo(p[1], CF(-0.308961034f, 0.698167443f), tol);
+  r &= isCloseTo(p[2], CF(-0.438901573f, 0.239981338f), tol);
+  r &= z[0] == CF(inf, 0.f);
+  r &= z[1] == CF(inf, 0.f);
+  r &= z[2] == CF(inf, 0.f);
+  r &= pd.getNumFinitePoles() == 6;
   r &= pd.getNumFiniteZeros() == 0;
 
 
