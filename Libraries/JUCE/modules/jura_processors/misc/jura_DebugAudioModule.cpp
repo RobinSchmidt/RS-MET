01280d566ffd92b2beebeb5b2ad1637d2b0e700e
Robin Schmidt <robin@rs-met.com>
2017-12-27 11:38:07 +0100

    added code to DebugAudioModule

--- Libraries/JUCE/modules/jura_processors/misc/jura_DebugAudioModule.cpp
+++ Libraries/JUCE/modules/jura_processors/misc/jura_DebugAudioModule.cpp
@@ -8,19 +8,33 @@ DebugAudioModule::DebugAudioModule(CriticalSection *lockToUse) : AudioModule(loc
 void DebugAudioModule::createParameters()
 {
   ScopedLock scopedLock(*lock);
+
+  outParam = new MetaControlledParameter("Output" , -1.0, 1.0, 0.0, Parameter::LINEAR, 0.01);
+  outParam->setValueChangeCallback<DebugAudioModule>(this, &DebugAudioModule::setOutputValue);
+  addObservedParameter(outParam);
 }
 
 void DebugAudioModule::processBlock(double **inOutBuffer, int numChannels, int numSamples)
 {
+  for(int i = 0; i < numChannels; i++)
+    for(int n = 0; n < numSamples; n++)
+      inOutBuffer[i][n] = outValue;
+  // todo: loop over child modules and let them add their value
+}
 
+void DebugAudioModule::processStereoFrame(double *left, double *right)
+{
+  *left = *right = outValue;
 }
 
 void DebugAudioModule::setSampleRate(double newSampleRate)
 {
- 
+  ScopedLock scopedLock(*lock);
+  AudioModule::setSampleRate(newSampleRate);
 }
 
 void DebugAudioModule::reset()
 {
-
+  ScopedLock scopedLock(*lock);
+  AudioModule::reset();
 }
--- Libraries/JUCE/modules/jura_processors/misc/jura_DebugAudioModule.h
+++ Libraries/JUCE/modules/jura_processors/misc/jura_DebugAudioModule.h
@@ -14,10 +14,20 @@ public:
 
   // overriden from AudioModule baseclass:
   virtual void processBlock(double **inOutBuffer, int numChannels, int numSamples) override;
+  virtual void processStereoFrame(double *left, double *right) override;
   virtual void setSampleRate(double newSampleRate) override; 
   virtual void reset() override;
 
+  // callback target functions:
+  void setOutputValue(double newValue) 
+  { 
+    outValue  = newValue; 
+  }
+
 protected:
+
+  double outValue;
+  MetaControlledParameter *outParam;
 
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DebugAudioModule)
