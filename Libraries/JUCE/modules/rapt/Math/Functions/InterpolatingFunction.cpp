49466ec0a515d8ba60b7a9bef94ff67015e2b060
Robin Schmidt <robin@rs-met.com>
2017-12-30 19:01:52 +0100

    added code to rsInterpolatingFunction

--- Libraries/JUCE/modules/rapt/Math/Functions/InterpolatingFunction.cpp
+++ Libraries/JUCE/modules/rapt/Math/Functions/InterpolatingFunction.cpp
@@ -1,7 +1,17 @@
 template<class T>
 void rsInterpolatingFunction<T>::addDataPoint(T x, T y)
 {
+  xValues.push_back(x);
+  yValues.push_back(y);
 
+  // keep arrays sorted according to ascending x:
+  size_t i = xValues.size()-1;
+  while(i > 0 && xValues[i] < xValues[i-1])
+  {
+    rsSwap(xValues[i-1], xValues[i]);
+    rsSwap(yValues[i-1], yValues[i]);
+    i--;
+  }
 }
 
 template<class T>
@@ -11,7 +21,22 @@ void rsInterpolatingFunction<T>::removeDataPoint(size_t index)
 }
 
 template<class T>
-void rsInterpolatingFunction<T>::moveDataPoint(size_t index, T newX, T newY)
+void rsInterpolatingFunction<T>::moveDataPoint(size_t i, T newX, T newY)
 {
+  xValues[i] = newX;
+  yValues[i] = newY;
 
+  // keep arrays sorted according to ascending x:
+  while(i > 0 && xValues[i] < xValues[i-1])
+  {
+    rsSwap(xValues[i-1], xValues[i]);
+    rsSwap(yValues[i-1], yValues[i]);
+    i--;
+  }
+  while(i < xValues.size()-1 && xValues[i] > xValues[i+1])
+  {
+    rsSwap(xValues[i+1], xValues[i]);
+    rsSwap(yValues[i+1], yValues[i]);
+    i++;
+  }
 }
--- Libraries/JUCE/modules/rapt/Math/Functions/InterpolatingFunction.h
+++ Libraries/JUCE/modules/rapt/Math/Functions/InterpolatingFunction.h
@@ -21,15 +21,56 @@ public:
   /** Moves an existing datapoint with given index to a new position. */
   void moveDataPoint(size_t index, T newX, T newY);
 
+  /** Returns a reference to our array of x-values. It's a constant reference because client code
+  is not allowed to edit that data directly. Instead, it must use the moveDataPoint function which
+  will update the datapoint and do some additional stuff. */ 
+  const std::vector<T>& getValuesX() { return xValues; }
 
+  /** Returns a (constant) reference to our array of x-values. @see getValuesX */
+  const std::vector<T>& getValuesY() { return yValues; }
+
+  /** Returns the first index i in our x-array such that x[i] > xToFind. */
+  size_t firstIndexOfGreaterX(T xToFind)
+  {
+    for(size_t i = 0; i < xValues.size(); i++)
+      if(xValues[i] > xToFind)
+        return i;
+    return xValues.size()-1;
+  }
+
+
+  /** For internal use only... */
+  T getValueLinear(T x, size_t i)
+  {
+    T x1 = xValues[i];
+    T y1 = yValues[i];
+    T x2 = xValues[i+1];
+    T y2 = yValues[i+1];
+    T t  = x - x1;
+    T thresh = 1.e-13; // todo: use epsilon of T
+    if(fabs(x2-x1) < thresh)
+      return T(0.5) * (y1+y2);
+    return x1 + t * (y2-y1) / (x2-x1); // check this formula
+  }
+
+  /** Returns an interpolated y-value at the given value of x. */
   T getValue(T x)
   {
     return x; // preliminary
+
+    if(xValues.size() == 0)
+      return 0;
+    if(x < xValues[0])
+      return xValues[0];
+    size_t i = firstIndexOfGreaterX(x);
+    if(i == xValues.size()-1)
+      return xValues[i];
+    return getValueLinear(x, i-1);
   }
 
 protected:
 
-  std::vector<T> x, y;
+  std::vector<T> xValues, yValues;
 
 };
 
