abcefa00397021088baca5116e15f978faa2acd7
Robin Schmidt <robin@rs-met.com>
2018-01-15 21:10:02 +0100

    added some test to UnitTestParameter::runTestParameter

--- Products/AudioApplications/TestAppJURA/Source/Tests/jura_framework/UnitTestsParameter.cpp
+++ Products/AudioApplications/TestAppJURA/Source/Tests/jura_framework/UnitTestsParameter.cpp
@@ -8,17 +8,35 @@ void UnitTestParameter::runTest()
   runTestModulatableParameter();
 }
 
+void UnitTestParameter::callbackTargetDouble(double value)
+{
+  lastCallbackValue = value;
+  numCallbacksReceived++;
+}
+
 void UnitTestParameter::runTestParameter()
 {
   beginTest("Parameter");
 
-  jura::Parameter p("TestParam");
+  numCallbacksReceived = 0;
+  numNotificationsReceived = 0; 
 
-  p.setValue(0.25, false, false);
-  expectEquals(p.getValue(), 0.25, "Parameter::getValue failed");
+  jura::Parameter p("TestParam", 0.0, 10.0, 5.0);
+  p.setValueChangeCallback<UnitTestParameter>(this, &UnitTestParameter::callbackTargetDouble);
+  expectEquals(numCallbacksReceived, 1,   "");
+
+  p.setValue(2.5, false, false);
+  expectEquals(p.getValue(),           2.5,  "Parameter::getValue failed");
+  expectEquals(p.getNormalizedValue(), 0.25, "Parameter::getNormalizedValue failed");
+  expectEquals(numCallbacksReceived,   1, "");
+
+  p.setValue(7.5, true, true);
+  expectEquals(p.getValue(),           7.5,  "Parameter::getValue failed");
+  expectEquals(p.getNormalizedValue(), 0.75, "Parameter::getNormalizedValue failed");
+  expectEquals(numCallbacksReceived,   2,   "");
+  expectEquals(lastCallbackValue,      7.5, "");
 
   // test, if callbacks and notifications work correctly
-
 
   int dummy = 0;
 }
--- Products/AudioApplications/TestAppJURA/Source/Tests/jura_framework/UnitTestsParameter.h
+++ Products/AudioApplications/TestAppJURA/Source/Tests/jura_framework/UnitTestsParameter.h
@@ -15,12 +15,28 @@ public:
 
   virtual void runTest() override;
 
+  /** Used as target function for the callback in parameter objects. */
+  void callbackTargetDouble(double value);
+
 protected:
 
   void runTestParameter();
   void runTestSmoothableParameter();
   void runTestMetaControlledParameter();
   void runTestModulatableParameter();
+
+  double lastCallbackValue;
+  int numCallbacksReceived;
+  int numNotificationsReceived;
+
+  juce::CriticalSection lock;
+
+  // managers:
+  //jura::rsSmoothingManager   smoothingManager;
+  //jura::MetaParameterManager metaManager;
+  //jura::ModulationManager    modManager(&lock); // doesn't work
+
+
 
 };
 
