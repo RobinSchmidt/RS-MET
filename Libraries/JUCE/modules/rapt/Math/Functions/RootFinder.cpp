f27c74cef7ff8978885aa915c1e5ab9a7f2cd3d9
Robin Schmidt <robin@rs-met.com>
2018-01-17 21:49:45 +0100

    rsRootFinder: implemented bisection (needs testing)

--- Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.cpp
+++ Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.cpp
@@ -1,7 +1,14 @@
 template<class Tx, class Ty>
-Tx rsRootFinder<Tx, Ty>::bisection(std::function<Ty(Tx)>& func, Tx xLeft, Tx xRight, Ty y)
+Tx rsRootFinder<Tx, Ty>::bisection(std::function<Ty(Tx)>& f, Tx xL, Tx xR, Ty y)
 {
-	
-
-  return xLeft; // preliminary
+  T tol = T(0.000001);         // tolerance, make parameter..and/or use machine epsilon
+  while( xR-xL > tol ) {
+    T xM  = T(0.5) * (xL+xR);  // x in the middle betwen xL, xR
+    T yM  = f(xM);             // function value at xM
+    if( yM > y ) 
+      xL = xM;
+    else
+      xR = xM; 
+  }
+  return T(0.5) * (xL+xR);
 }
