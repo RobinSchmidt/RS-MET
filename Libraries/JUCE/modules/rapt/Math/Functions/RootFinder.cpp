c66af1f41049bd537c2dc782a87b54debd318dd7
Robin Schmidt <robin@rs-met.com>
2018-01-18 15:20:33 +0100

    changed comments for root finder, added stub for false position method

--- Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.cpp
+++ Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.cpp
@@ -1,30 +1,38 @@
 template<class Tx, class Ty>
 Tx rsRootFinder<Tx, Ty>::bisection(std::function<Ty(Tx)>& f, Tx xL, Tx xR, Ty y)
 {
-  Tx eps = std::numeric_limits<Tx>::epsilon();
-  Ty fL  = f(xL) - y;  
-  Tx xM  = Tx(0.5)*(xL+xR);
-  int its = 0;                // iteration counter - for development
+  static const int maxNumIterations = 60; // should be enough for double-precision
+  int its = 0;                            // iteration counter
+  Tx eps  = std::numeric_limits<Tx>::epsilon();
+  Ty fL   = f(xL) - y;  
+  Tx xM   = Tx(0.5)*(xL+xR);
   while(xR-xL > fabs(xM*eps)) {
     Ty fM = f(xM) - y;
     if(fL*fM > 0) { xL = xM; fL = fM; }
     else          { xR = xM;          }
     xM = Tx(0.5)*(xL+xR);
-    its++; }
-  return xM;
-  // maybe have a maximum number of iterations after which to return and maybe trigger an assertion
-  // when it is exceeded
-
-  /*
-  // too simple - works only for ascending functions:
-  while( xR-xL > tol ) {
-    Tx xM  = Tx(0.5) * (xL+xR);  // x in the middle betwen xL, xR
-    Ty yM  = f(xM);              // function value at xM
-    if( yM > y ) 
-      xL = xM;
-    else
-      xR = xM; 
+    if(its++ > maxNumIterations) {
+      //rsError("Bisection failed to converge");
+      break; }
   }
-  */
-  //return Tx(0.5) * (xL+xR);
+  return xM;
+}
+
+// move to somewhere else:
+template<class T>
+inline T lineZeroCrossing(T xL, T yL, T xR, T yR)
+{
+  T a = (yR-yL)/(xR-xL); // slope
+  T b = yL-a*xL;         // offset
+  return -b/a;
+
+  // maybe this can be simplified algebraically? ..like this:
+  //return xL - yL*(xR-xL)/(yR-yL);
+}
+
+template<class Tx, class Ty>
+Tx rsRootFinder<Tx, Ty>::falsePosition(std::function<Ty(Tx)>& func, Tx xLeft, Tx xRight, Ty y)
+{
+
+  return xLeft; // preliminary
 }
--- Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.h
+++ Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.h
@@ -6,17 +6,35 @@ solutions to the equation f(x) = 0 for an arbitrary given function f(x). For mor
 right hand side does not actually need to be equal to zero but can be any target value, so it 
 actually find solutions to f(x) = y for given f(x) and given y. This little addition to standard 
 textbook root-finding algorithms doesn't change much algorithmically (we just need to subtract the 
-target value in each function evaluation), yet adds a lot to the flexibility. */
+target value in each function evaluation), yet adds a lot to the flexibility. Some algorithms 
+require the user to pass an initial interval that is assumed to bracket the root, others require an
+initial estimate of the root.
+
+References
+(1) Numerical Recipies in C (2nd Edition), Chapter 9
+
+*/
 
 template<class Tx, class Ty>
 class rsRootFinder
 {
 public:
 
-  /** Given a function and two x-values xLeft, xRight that bracket the root, this function finds
-  the root via the bisection algorithm. For more generality, the target-value for y does not need
-  to be zero but can be passed as additional parameter. */
+  /** Bisection takes an initial interval xLeft, xRight (assumed to bracket the root) and evaluates 
+  the function at the midpoint of the interval. Depending on the function value, the midpoint 
+  becomes either the new left or the new right border of the bracketing interval. So, in each 
+  iteration, the size of the interval is halved. The order of convergence is linear (in each 
+  iteration, we get one more correct binary digit in the root estimate) and convergence is 
+  guaranteed. */
   static Tx bisection(std::function<Ty(Tx)>& func, Tx xLeft, Tx xRight, Ty y = 0);
+
+  /** Similar to bisection but doesn't use the midpoint of the current bracketing interval, but the
+  point where a line between (xLeft,yLeft), (xRight,yRight) crosses the x-axis. Convergence is
+  typically (for smooth functions, that are well approxiamted by a line near the root) faster than
+  for bisection and convergence is guranteed. */
+  static Tx falsePosition(std::function<Ty(Tx)>& func, Tx xLeft, Tx xRight, Ty y = 0);
+
+
   
   //Tx secant(Tx xLeft, Tx xRight, Ty y = 0);
   
