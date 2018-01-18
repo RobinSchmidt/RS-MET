412ef5bb700a3866662649162de41ed53d7c2a9c
Robin Schmidt <robin@rs-met.com>
2018-01-18 15:56:52 +0100

    fixed falsePosition root finder convergence

--- Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.cpp
+++ Libraries/JUCE/modules/rapt/Math/Functions/RootFinder.cpp
@@ -22,33 +22,29 @@ Tx rsRootFinder<Tx, Ty>::bisection(std::function<Ty(Tx)>& f, Tx xL, Tx xR, Ty y)
 template<class T>
 inline T lineZeroCrossing(T xL, T yL, T xR, T yR)
 {
-  T a = (yR-yL)/(xR-xL); // slope
-  T b = yL-a*xL;         // offset
-  return -b/a;
+  return xL - yL*(xR-xL)/(yR-yL);
 
-  // maybe this can be simplified algebraically? ..like this:
-  //return xL - yL*(xR-xL)/(yR-yL);
+  //// old - code above is algebraically simplified:
+  //T a = (yR-yL)/(xR-xL); // slope
+  //T b = yL-a*xL;         // offset
+  //return -b/a;
 }
 
 template<class Tx, class Ty>
 Tx rsRootFinder<Tx, Ty>::falsePosition(std::function<Ty(Tx)>& f, Tx xL, Tx xR, Ty y)
 {
   static const int maxNumIterations = 60; // should be enough for double-precision
-  Tx tol  = 4*std::numeric_limits<Tx>::epsilon();
-  Ty fL   = f(xL) - y;
-  Ty fR   = f(xR) - y;
-  Tx xM;
-  Ty fM;
-  for(int i = 1; i <= maxNumIterations; i++)
-  {
+  Tx tol = 4*std::numeric_limits<Tx>::epsilon();
+  Ty fL  = f(xL) - y;
+  Ty fR  = f(xR) - y;
+  Tx xM; Ty fM;
+  for(int i = 1; i <= maxNumIterations; i++) {
     xM = lineZeroCrossing(xL, fL, xR, fR);
-    if(xR-xL <= fabs(xM*tol)) 
-      return xM; // done
     fM = f(xM) - y;
-    if(fL*fM > 0) { 
-      xL = xM; fL = fM; }
-    else { 
-      xR = xM; fR = fM; }
+    if(fM == 0 || xR-xL <= fabs(xM*tol)) 
+      return xM; // done
+    if(fL*fM > 0) { xL = xM; fL = fM; }
+    else          { xR = xM; fR = fM; }
   }
   //rsError("rsRootFinder::falsePosition failed to converge");
   return xM;
