bf6a4fd053aa8e36e988c7f3b60c849176e8ec97
Robin Schmidt <robin@rs-met.com>
2018-01-27 07:14:46 +0100

    added code to rsLadderFilterStereo

--- Libraries/JUCE/modules/rosic/filters/rosic_LadderFilter.h
+++ Libraries/JUCE/modules/rosic/filters/rosic_LadderFilter.h
@@ -10,13 +10,38 @@ class rsLadderFilterMono : public RAPT::rsLadderFilter<double, double>
 
 };
 
+*/
+
+//=================================================================================================
+
+/** A ladder filter based on instantiating the template ladder filter with the "rsFloat64x2" SSE2 
+wrapper for the signal and "double" for the parameters (cutoff, etc). This gives two parallel
+ladder filters with the same settings for the price of one. */
+
 class rsLadderFilterStereo : public RAPT::rsLadderFilter<rsFloat64x2, double>
 {
 
-};
-*/
+public:
 
-class rsLadderFilter : public RAPT::rsLadderFilter<rsFloat64x2, rsFloat64x2>
+  INLINE void getSampleFrameStereo(double* left, double* right)
+  {
+    rsFloat64x2 tmp = getSample(rsFloat64x2(*left, *right));
+    *left  = tmp[0];
+    *right = tmp[1];
+  }
+
+};
+
+//=================================================================================================
+
+/** A ladder filter based on instantiating the template ladder filter with the rsFloat64x2 SSE2 
+wrapper for both, the signal and the parameters (cutoff, etc.). This gives two parallel ladder
+filters with potentially different settings for the price of one, as far as the audio processing 
+functions (getSample, etc.) are concerned. Setting paramaters (setCutoff, etc.) will still cost as 
+much as if there would be two separate filters because the coefficient compuations can't be 
+vectorized (yet?) and require to fall back to scalar code. */
+
+class rsLadderFilter : public RAPT::rsLadderFilter<rsFloat64x2, rsFloat64x2> // maybe rename
 {
 
 public:
@@ -24,6 +49,7 @@ public:
   // we need to replicate the interface of RAPT::rsLadderFilter but with double for the parameter
   // types and convertd to rsFloat64x2 as necessarry, also take into account stereo spread for the
   // cutoff frequency
+  // maybe do not inherit publically but protected
 
   void setStereoSpread(double newSpread) { stereoSpread = newSpread; }
     // more to do
@@ -33,6 +59,10 @@ protected:
   double stereoSpread = 0.0;
 
 };
+
+
+
+
 
 }
 
