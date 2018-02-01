c6520ca51bcf2a58fd7077a25b36f39e14fe52ff
Robin Schmidt <robin@rs-met.com>
2018-02-01 16:46:08 +0100

    moved poly-classes into jura_framework

--- Libraries/JUCE/modules/jura_processors/jura_processors.h
+++ Libraries/JUCE/modules/jura_processors/jura_processors.h
@@ -136,9 +136,6 @@ namespace jura
 #include "misc/jura_DebugAudioModule.h"
 #include "misc/jura_ToolChain.h"
 
-#include "unfinished/jura_PolyModule.h"
-#include "unfinished/jura_PolySlot.h"
-#include "unfinished/jura_PolyModulators.h"
 #include "unfinished/jura_QuadSource.h"
 #include "unfinished/jura_DualFilter.h"
 #include "unfinished/jura_NewSynth.h"
