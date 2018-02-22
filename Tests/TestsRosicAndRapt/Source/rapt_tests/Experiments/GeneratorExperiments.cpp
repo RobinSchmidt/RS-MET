704f3766590ca8a1d2d7d1185c55c27efe059f9b
Robin Schmidt <robin@rs-met.com>
2018-02-22 05:13:40 +0100

    implemented getDxDy for moore curve

--- Tests/TestsRosicAndRapt/Source/rapt_tests/Experiments/GeneratorExperiments.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/Experiments/GeneratorExperiments.cpp
@@ -281,21 +281,100 @@ void d2xy(int n, int d, int *x, int *y)  // convert d to (x,y)
   for (s=1; s<n; s*=2) {
     rx = 1 & (t/2);
     ry = 1 & (t ^ rx);
-    rot(s, x, y, rx, ry);
+    rot(s, x, y, rx, ry);    
+    //rot(s, x, y, ry, rx);  // wrong, but might be interesting for audio
     *x += s * rx;
     *y += s * ry;
     t /= 4;
   }
 }
+
+// modified variant (for closed loop - doesn't work yet):
+void rotm(int n, int *x, int *y, int rx, int ry)
+{
+  if (ry == 1) {         // 0 
+    if (rx == 1) {       // 1
+      *x = n-1 - *x;
+      *y = n-1 - *y;
+    }
+
+    ////Swap x and y
+    //int t  = *x;
+    //*x = *y;
+    //*y = t;
+
+  }
+}
+void d2xym(int n, int d, int *x, int *y) 
+{
+  int rx, ry, s, t=d;
+  *x = *y = 0;
+  //*x = (int) sqrt(n);
+  for(s=1; s<n; s*=2)   // loop over the steps (this is a shlemiel algo :-O)
+  {
+    //rx = 1 & (t/2);
+    //ry = 1 & (t ^ rx);
+    //rotm(s, x, y, rx, ry);
+
+    ry = 1 & (t/2);
+    rx = 1 & (t ^ ry);
+    rot(s, x, y, ry, rx);
+
+    *x += s * rx;
+    *y += s * ry;
+    t /= 4;
+  }
+}
+
+void getDxDy(int& dx, int& dy, int i, int n) //
+{
+  // dx and dy can both be -1, 0, +1, the tricky part is figure out which values they should
+  // have as function of i and n, for order k = 2, n = 4^k = 16, we should have:
+  //  i: 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+  // dx: -1  0   1   0   -1  0   1   1   1   0  -1  0   1   0   -1  -1 
+  // dy: 0   1   0   1   0   1   0   0   0   -1 0   -1  0   -1  0   0 
+
+  // hmm...maybe it's easier to use the current x,y coordinates as input values too?
+
+  // works for n=16 ...but does it generalize?
+
+  dx = dy = 0;
+  int n2 = n /2;
+  //int n4 = n2/2;
+
+  if(i % 4 == 0)       { dx = -1; dy =  0; }  // left
+  else if(i % 4 == 1)  { dx =  0; dy = +1; }  // up
+  else if(i % 4 == 2)  { dx = +1; dy =  0; }  // right
+  else if(i % 4 == 3)  { dx =  0; dy = +1; }  // up
+
+  if(i == n2-1) { dx = 1;   dy = 0;   }       // this is also a swap of dx,dy
+  if(i >= n2)   { dx = -dx; dy = -dy; }
+}
+
+
 void hilbertCurve()
 {
-  int order = 4;
+  int order = 2;
   int n = (int) pow(4, order); // wiki says, it could be a power of 2 (but then it'a not a square)
   int max = (int) pow(2, order);
 
   std::vector<int> x(n), y(n);
+  //for(int d = 0; d < n; d++)
+  //  d2xy(n, d, &x[d], &y[d]);
+
   for(int d = 0; d < n; d++)
-    d2xy(n, d, &x[d], &y[d]);
+    d2xym(n, d, &x[d], &y[d]);
+
+  // experimental - try to make a Moore curve (closed Hilbert curve)
+  int xi = 0, yi = 0;
+  int dx = 0, dy = 0;
+  for(int i = 0; i < n; i++)
+  {
+    x[i] = xi; y[i] = yi;
+    getDxDy(dx, dy, i, n);
+    xi += dx; yi += dy;
+  }
+
 
   GNUPlotter plt;
   plt.addDataArrays(n, &x[0], &y[0]);
@@ -303,6 +382,16 @@ void hilbertCurve()
   plt.setPixelSize(400, 400);
   plt.addCommand("set size square");  // set aspect ratio to 1:1
   plt.plot();
+
+  // see:
+  // https://www.reddit.com/r/visualizedmath/comments/7xtxgb/hilbert_curve/
+  // http://www4.ncsu.edu/~njrose/pdfFiles/HilbertCurve.pdf
+  // http://www.fundza.com/algorithmic/space_filling/hilbert/basics/index.html
+  // https://en.wikipedia.org/wiki/Hilbert_curve
+  // https://en.wikipedia.org/wiki/Moore_curve
+  // https://github.com/adishavit/hilbert
+  // https://marcin-chwedczuk.github.io/iterative-algorithm-for-drawing-hilbert-curve
+  // http://wwwmayr.informatik.tu-muenchen.de/konferenzen/Jass05/courses/2/Valgaerts/Valgaerts_paper.pdf
 }
 
 void xoxosOsc()
