98091c381c4857830d1f3a5f057e920d6318f3a7
Robin Schmidt <robin@rs-met.com>
2018-03-01 14:24:10 +0100

    added code to FiniteAutomaton

--- Tests/TestsRosicAndRapt/Source/Shared/Prototypes/FiniteAutomaton.cpp
+++ Tests/TestsRosicAndRapt/Source/Shared/Prototypes/FiniteAutomaton.cpp
@@ -2,14 +2,25 @@
 
 std::string FiniteAutomaton::readSymbol(char c)
 {
+  // -find index i belonging to c (maybe have a size 255 array of ints with entries that
+  //  are all -1 by default)
+  // -if i is in the valid range:
+  //  -read out the transitions matrix at [state][i] to update state: 
+  //   state = transitions[state][i];
+  //  -return stateOutputs[i]
 
   return std::string(); // return empty string by default
 }
 
 std::string FiniteAutomaton::translate(const std::string& input)
 {
+  int tmpState = state;
+
+  state = 0;
   std::string s;
   for(size_t i = 0; i < input.size(); i++)
     s += readSymbol(input[i]);
+
+  state = tmpState; // we dont want a call to translate affect the state member
   return s;
 }
--- Tests/TestsRosicAndRapt/Source/Shared/Prototypes/FiniteAutomaton.h
+++ Tests/TestsRosicAndRapt/Source/Shared/Prototypes/FiniteAutomaton.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <string>
 
-/** */
+/** Implements a finite automaton (aka state machine?). */
 
 class FiniteAutomaton
 {
@@ -15,18 +15,29 @@ public:
 
   //void setState
 
+  /** Returns a string representation of this automaton suitable for storing the automaton 
+  definition in a file. */
+  void getAsString();
 
+  /** Sets this automaton up from a defining string. */
+  void setupFromString(const std::string& definition);
+
+  /** Perform the state transition associated with the given symbol and returns the output string 
+  that is associated with the new state. */
   std::string readSymbol(char c);
 
+  /** Translates the given string input string to the corresponding output string. This can also be 
+  called in the middle of another translation without affecting our state. */
   std::string translate(const std::string& input);
 
+  /** Resets the current state into the start state (index 0). */
   void reset() { state = 0; }
 
 protected:
 
   std::vector<std::string> stateNames;
   std::vector<std::string> stateOutputs;
-  std::vector<char> alphabet;
+  std::vector<char> alphabet; // input alphabet
   //rsMatrix<int> transitions;
   int numStates = 0;
   int numSymbols = 0;
