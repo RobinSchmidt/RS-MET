ff0c625155edddb575900191c000bd382ca71827
Robin Schmidt <robin@rs-met.com>
2018-03-17 11:40:15 +0100

    added text to Ideas.h

--- Tests/TestsRosicAndRapt/Source/Shared/Prototypes/Ideas.h
+++ Tests/TestsRosicAndRapt/Source/Shared/Prototypes/Ideas.h
@@ -12,18 +12,37 @@
  -xor(a, b) = or(and(a,not(b)),and(b,not(a))) = a + b - a*b(3-a-b+a*b)
  -maybe these functions can be extended to take a 3rd argument c for the correlation between 
   a and b
-  -and(a,b,c) should reduce to the form above when c=0, 
-   when: c=1: and(a=1,b=1,1) = 0, and(a=1,b=0,1) = 0
-  -when a formula for "and" is established, a formual for "or" can be derivedn from 
-   or(a,b) = not(not(a),not(b))
+  -and(a,b,c) should reduce to the form above when c=0: and(a,b,0) = a*b
+   when c = +1: and(a=1,b=0,+1) = and(a=0,b=1,+1) = 0
+   when c = -1: and(a=1,b=1,-1) = and(a=0,b=0,-1) = 0
+   ...maybe we can find more conditions that must hold and derive a general formula for and(a,b,c)
+  -when a formula for "and" is established, a formula for "or" can be derived from 
+   or(a,b) = not(and(not(a),not(b)))
   -maybe it should also output two new correlation values for (a,result), (b,result) as side 
    results
+ -maybe a class for such variables can be written, using operators "*" for "and" and "+" for "or"
+  -can we then also derive meaningful formulas for "/" and "-"?
+ -...maybe using p,q instead of a,b is a nicer convention
 
 -3 valued logic: true,false,unknown
  T and U = U, T or U = T, F and U = F, F or U = U, U and U = U, U or U = U
  the rest stays the same (F and T = F etc.)
- -can be modelled by probabilistic logic by using 0.5 as "unknown" - maybe by clamping unknown
+ -can (?) be modelled by probabilistic logic by using 0.5 as "unknown" - maybe by clamping unknown
   results to 0.5
+
+-use that probabilistic logic in a cellular automaton - the new activation of each cell is a 
+ logical combination of its and neighbour activations
+ -read out the celluar automaton one cell per sample, after each cycle compute new cell activations
+ -have a global "fuzziness" parameter that crossfades all cell-activtions between hard 0/1 and 0.5
+ -maybe lowpass filter cell contents during readout
+ -maybe use a 2D automaton with various readout rules (scan lines, diagonal, hilbert-curve, etc.)
+ -needs on-the-fly sample-rate conversion - read out the automaton always at its "natural" rate and
+  then convert to target sample-rate
+ -maybe neighbour correlations can be computed on the fly and be fed into the and/or operations 
+  (maybe scaled by a factor)
+ -the cells could be initialized with random values, maybe with an optional "balancing" of 
+  0s and 1s: just count the 1s, and if there are n too many, change n randomly chosen 1-cells to 0
+  (or the other way around)
 */
 
 #endif
