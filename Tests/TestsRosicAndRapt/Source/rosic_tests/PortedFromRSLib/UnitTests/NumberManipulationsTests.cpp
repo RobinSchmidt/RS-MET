7aaadbda65aaf5b2a5694f8144d09e25ff614145
Robin Schmidt <robin@rs-met.com>
2018-05-09 23:04:53 +0200

    made NumberManipulationsTests.cpp compile

--- Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/UnitTests/NumberManipulationsTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/UnitTests/NumberManipulationsTests.cpp
@@ -18,41 +18,41 @@ bool testDoubleIntConversions(std::string &reportString)
   bool testResult = true;
 
   int  x;
-  x = rsRoundToInt( 1.2); testResult &= (x==1);
-  x = rsRoundToInt( 1.8); testResult &= (x==2);
-  x = rsRoundToInt( 1.5); testResult &= (x==2);
-  x = rsRoundToInt( 2.5); testResult &= (x==3);
-  x = rsRoundToInt(-1.2); testResult &= (x==-1);
-  x = rsRoundToInt(-1.8); testResult &= (x==-2);
-  x = rsRoundToInt(-1.5); testResult &= (x==-1);
-  x = rsRoundToInt(-2.5); testResult &= (x==-2);
+  x = roundToInt( 1.2); testResult &= (x==1);
+  x = roundToInt( 1.8); testResult &= (x==2);
+  x = roundToInt( 1.5); testResult &= (x==2);
+  x = roundToInt( 2.5); testResult &= (x==3);
+  x = roundToInt(-1.2); testResult &= (x==-1);
+  x = roundToInt(-1.8); testResult &= (x==-2);
+  x = roundToInt(-1.5); testResult &= (x==-1);
+  x = roundToInt(-2.5); testResult &= (x==-2);
 
-  x = rsFloorInt( 1.2); testResult &= (x==1);
-  x = rsFloorInt( 1.8); testResult &= (x==1);
-  x = rsFloorInt( 1.5); testResult &= (x==1);
-  x = rsFloorInt( 2.5); testResult &= (x==2);
-  x = rsFloorInt(-1.2); testResult &= (x==-2);
-  x = rsFloorInt(-1.8); testResult &= (x==-2);
-  x = rsFloorInt(-1.5); testResult &= (x==-2);
-  x = rsFloorInt(-2.5); testResult &= (x==-3);
+  x = floorInt( 1.2); testResult &= (x==1);
+  x = floorInt( 1.8); testResult &= (x==1);
+  x = floorInt( 1.5); testResult &= (x==1);
+  x = floorInt( 2.5); testResult &= (x==2);
+  x = floorInt(-1.2); testResult &= (x==-2);
+  x = floorInt(-1.8); testResult &= (x==-2);
+  x = floorInt(-1.5); testResult &= (x==-2);
+  x = floorInt(-2.5); testResult &= (x==-3);
 
-  x = rsCeilInt( 1.2); testResult &= (x==2);
-  x = rsCeilInt( 1.8); testResult &= (x==2);
-  x = rsCeilInt( 1.5); testResult &= (x==2);
-  x = rsCeilInt( 2.5); testResult &= (x==3);
-  x = rsCeilInt(-1.2); testResult &= (x==-1);
-  x = rsCeilInt(-1.8); testResult &= (x==-1);
-  x = rsCeilInt(-1.5); testResult &= (x==-1);
-  x = rsCeilInt(-2.5); testResult &= (x==-2);
+  x = ceilInt( 1.2); testResult &= (x==2);
+  x = ceilInt( 1.8); testResult &= (x==2);
+  x = ceilInt( 1.5); testResult &= (x==2);
+  x = ceilInt( 2.5); testResult &= (x==3);
+  x = ceilInt(-1.2); testResult &= (x==-1);
+  x = ceilInt(-1.8); testResult &= (x==-1);
+  x = ceilInt(-1.5); testResult &= (x==-1);
+  x = ceilInt(-2.5); testResult &= (x==-2);
 
-  x = rsTruncateToInt( 1.2); testResult &= (x==1);
-  x = rsTruncateToInt( 1.8); testResult &= (x==1);
-  x = rsTruncateToInt( 1.5); testResult &= (x==1);
-  x = rsTruncateToInt( 2.5); testResult &= (x==2);
-  x = rsTruncateToInt(-1.2); testResult &= (x==-1);
-  x = rsTruncateToInt(-1.8); testResult &= (x==-1);
-  x = rsTruncateToInt(-1.5); testResult &= (x==-1);
-  x = rsTruncateToInt(-2.5); testResult &= (x==-2);
+  x = truncateToInt( 1.2); testResult &= (x==1);
+  x = truncateToInt( 1.8); testResult &= (x==1);
+  x = truncateToInt( 1.5); testResult &= (x==1);
+  x = truncateToInt( 2.5); testResult &= (x==2);
+  x = truncateToInt(-1.2); testResult &= (x==-1);
+  x = truncateToInt(-1.8); testResult &= (x==-1);
+  x = truncateToInt(-1.5); testResult &= (x==-1);
+  x = truncateToInt(-2.5); testResult &= (x==-2);
 
 
   appendTestResultToReport(reportString, testName, testResult);
@@ -67,26 +67,26 @@ bool testExponentExtraction(std::string &reportString)
   int exponent;
 
   // double:
-  exponent = rsExtractExponentFromDouble(0.49999); testResult &= ( exponent == -2 );
-  exponent = rsExtractExponentFromDouble(0.99999); testResult &= ( exponent == -1 );
-  exponent = rsExtractExponentFromDouble(1.0);     testResult &= ( exponent ==  0 );
-  exponent = rsExtractExponentFromDouble(1.99999); testResult &= ( exponent ==  0 );
-  exponent = rsExtractExponentFromDouble(2.0);     testResult &= ( exponent ==  1 );
-  exponent = rsExtractExponentFromDouble(2.00001); testResult &= ( exponent ==  1 );
-  exponent = rsExtractExponentFromDouble(3.99999); testResult &= ( exponent ==  1 );
-  exponent = rsExtractExponentFromDouble(4.0);     testResult &= ( exponent ==  2 );
-  exponent = rsExtractExponentFromDouble(4.00001); testResult &= ( exponent ==  2 );
+  exponent = extractExponent(0.49999); testResult &= ( exponent == -2 );
+  exponent = extractExponent(0.99999); testResult &= ( exponent == -1 );
+  exponent = extractExponent(1.0);     testResult &= ( exponent ==  0 );
+  exponent = extractExponent(1.99999); testResult &= ( exponent ==  0 );
+  exponent = extractExponent(2.0);     testResult &= ( exponent ==  1 );
+  exponent = extractExponent(2.00001); testResult &= ( exponent ==  1 );
+  exponent = extractExponent(3.99999); testResult &= ( exponent ==  1 );
+  exponent = extractExponent(4.0);     testResult &= ( exponent ==  2 );
+  exponent = extractExponent(4.00001); testResult &= ( exponent ==  2 );
 
   // float:
-  exponent = rsExtractExponentFromFloat(0.49999f); testResult &= ( exponent == -2 );
-  exponent = rsExtractExponentFromFloat(0.99999f); testResult &= ( exponent == -1 );
-  exponent = rsExtractExponentFromFloat(1.0f);     testResult &= ( exponent ==  0 );
-  exponent = rsExtractExponentFromFloat(1.99999f); testResult &= ( exponent ==  0 );
-  exponent = rsExtractExponentFromFloat(2.0f);     testResult &= ( exponent ==  1 );
-  exponent = rsExtractExponentFromFloat(2.00001f); testResult &= ( exponent ==  1 );
-  exponent = rsExtractExponentFromFloat(3.99999f); testResult &= ( exponent ==  1 );
-  exponent = rsExtractExponentFromFloat(4.0f);     testResult &= ( exponent ==  2 );
-  exponent = rsExtractExponentFromFloat(4.00001f); testResult &= ( exponent ==  2 );
+  exponent = extractExponent(0.49999f); testResult &= ( exponent == -2 );
+  exponent = extractExponent(0.99999f); testResult &= ( exponent == -1 );
+  exponent = extractExponent(1.0f);     testResult &= ( exponent ==  0 );
+  exponent = extractExponent(1.99999f); testResult &= ( exponent ==  0 );
+  exponent = extractExponent(2.0f);     testResult &= ( exponent ==  1 );
+  exponent = extractExponent(2.00001f); testResult &= ( exponent ==  1 );
+  exponent = extractExponent(3.99999f); testResult &= ( exponent ==  1 );
+  exponent = extractExponent(4.0f);     testResult &= ( exponent ==  2 );
+  exponent = extractExponent(4.00001f); testResult &= ( exponent ==  2 );
 
   appendTestResultToReport(reportString, testName, testResult);
   return testResult;
--- Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/UnitTestsRSLib.cpp
+++ Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/UnitTestsRSLib.cpp
@@ -7,7 +7,7 @@ using namespace rosic;
 #include "UnitTests/FilterTests.cpp"
 #include "UnitTests/MiscAudioTests.cpp"
 #include "UnitTests/ModalTests.cpp"
-//#include "UnitTests/NumberManipulationsTests.cpp"
+#include "UnitTests/NumberManipulationsTests.cpp" 
 //#include "UnitTests/PitchDetectorTests.cpp"
 //#include "UnitTests/SortAndSearchTests.cpp"
 //#include "UnitTests/TypeSizeTests.cpp"
