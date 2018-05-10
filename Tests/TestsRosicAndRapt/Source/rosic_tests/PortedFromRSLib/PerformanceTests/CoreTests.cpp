484faa146e8f28eceb030a44feec379ad646a2de
Robin Schmidt <robin@rs-met.com>
2018-05-10 18:51:51 +0200

    made CoreTests.cpp compile

--- Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/PerformanceTests/CoreTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/PerformanceTests/CoreTests.cpp
@@ -4,32 +4,32 @@ void testFlagArray(std::string &reportString)
 {
   static const rsUint32 numFlags = 1000000;
 
-  ProcessorCycleCounter counter;
+  ::ProcessorCycleCounter counter;
   double cyclesPerFlag;
   int i;
 
   counter.init();
   rsFlagArray a(numFlags);
   cyclesPerFlag = (double) counter.getNumCyclesSinceInit() / numFlags;
-  appendResultToReport(reportString, "rsFlagArray, Creation", cyclesPerFlag);
+  printPerformanceTestResult("rsFlagArray, Creation", cyclesPerFlag);
 
   counter.init();
   rsUint64 dummy = a.getNumTrueFlags();
   cyclesPerFlag = (double) counter.getNumCyclesSinceInit() / numFlags;
-  appendResultToReport(reportString, "rsFlagArray, Count true flags", cyclesPerFlag);
+  printPerformanceTestResult("rsFlagArray, Count true flags", cyclesPerFlag);
 
   a.setAllFalse();
   counter.init();
   for(i = 0; i < numFlags; i++)
     a.setFlagTrue(i);
   cyclesPerFlag = (double) counter.getNumCyclesSinceInit() / numFlags;
-  appendResultToReport(reportString, "rsFlagArray, Write", cyclesPerFlag);
+  printPerformanceTestResult("rsFlagArray, Write", cyclesPerFlag);
 
   counter.init();
   bool b;
   for(i = 0; i < numFlags; i++)
     b = a.isFlagTrue(i);
   cyclesPerFlag = (double) counter.getNumCyclesSinceInit() / numFlags;
-  appendResultToReport(reportString, "rsFlagArray, Read", cyclesPerFlag);
+  printPerformanceTestResult("rsFlagArray, Read", cyclesPerFlag);
 }
 
--- Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/PerformanceTestsRSLib.cpp
+++ Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/PerformanceTestsRSLib.cpp
@@ -4,7 +4,7 @@ using namespace RAPT;
 using namespace rosic;
 
 #include "PerformanceTests/AnalysisTests.cpp"
-//#include "PerformanceTests/CoreTests.cpp"
+#include "PerformanceTests/CoreTests.cpp"
 //#include "PerformanceTests/MathTests.cpp"
 //#include "PerformanceTests/MiscAudioTests.cpp"
 //#include "PerformanceTests/ModalTests.cpp"
