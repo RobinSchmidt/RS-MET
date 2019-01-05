9ed1d54ecc32b80da81e6869535fb00953634880
Robin Schmidt <robin@rs-met.com>
2019-01-05 09:04:06 +0100

    fixed amplitude error in rsModalFilterFloatSSE2 unit test

--- Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/UnitTests/ModalTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/UnitTests/ModalTests.cpp
@@ -73,16 +73,14 @@ bool testModalFilter2(std::string &reportString)
   testResult &= err < 1.e-7;
     // 4 orders of magnitude less precise than rsModalFilterWithAttack (with GCC)
 
-  // check rsModalFilterFloatSSE2 - has wrong amplitude:
+  // check rsModalFilterFloatSSE2:
   rsModalFilterFloatSSE2 mf_f32x4;
   double scaledA = A*scaler;
-  mf_f32x4.setParameters(w, scaledA, p, ta*fs, ta*fs, 0.0, td*fs, td*fs, 0.0);
+  mf_f32x4.setParameters(w, scaledA, p, td2*fs, td2*fs, 0.0, td*fs, td*fs, 0.0);
   getImpulseResponse(mf_f32x4, x1, N);
-  plotData(N, 0, 1/fs, xt, x1); 
-  // the amplitude is too low - we need to incorporate the scaler ...but just using scaler*A seems
-  // to be wrong - why?
-
-
+  //plotData(N, 0, 1/fs, xt, x1); 
+  err = RAPT::rsArray::maxDeviation(xt, x1, N);
+  testResult &= err < 0.002;
 
   //cout << reportString;
   appendTestResultToReport(reportString, testName, testResult);
