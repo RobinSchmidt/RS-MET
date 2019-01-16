b6e6c5306fc935c40bd5cf09accbdc3d5f8c336f
Robin Schmidt <robin@rs-met.com>
2019-01-16 17:29:50 +0100

    added code to spectrogramUnitTest

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/MiscUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/MiscUnitTests.cpp
@@ -1,8 +1,43 @@
 bool testSpectrogramResynthesis(int blockSize, int hopSize, int signalLength, int fftSize, 
   int windowType = RAPT::rsWindowFunction::HANNING_WINDOW_ZN)
 {
-  bool r = true;  
+  bool r = true;
 
+  int B = blockSize;
+  int H = hopSize;
+  int N = signalLength;
+  int M = fftSize;
+
+  // generate the window function (todo: let the spectrogram class do that by itself):
+  std::vector<double> w(B);
+  RAPT::rsWindowFunction::createWindow(&w[0], B, windowType, true);
+
+  // generate a sequence of random numbers:
+  std::vector<double> x(N);
+  int n;
+  RAPT::rsNoiseGenerator<double> prng;
+  for(n = 0; n < N; n++)
+    x[n] = prng.getSample();
+
+  typedef RAPT::rsSpectrogram<double> SP; // spectrogram processor
+
+  // compute the complex spectrogram:
+  rsMatrix<rsComplexDbl> s = SP::complexSpectrogram(&x[0], N, &w[0], B, H, 1);
+  //int F = s.getNumRows();
+  // todo: let the function take an FFT-size parameter instead of a zero-padding factor (maybe)
+  // facilitates having an FFT size independent from the block-size
+
+  // resynthesize signal from spectrogram:
+  std::vector<double> y  = SP::synthesize(s, &w[0], B, H, &w[0]);
+
+  // check, if resynthesized matches original signal:
+  double tol = 1.e-13;
+  std::vector<double> err(N);
+  for(n = 0; n < N; n++)         // create error signal
+    err[n] = x[n] - y[n];
+  for(n = 0; n < N; n++)
+    r &= abs(err[n]) <= tol;
+  //plotVector(err);  // may be uncommented to plot error signal, if something is going wrong
 
   return r;
 }
@@ -12,7 +47,10 @@ bool spectrogramUnitTest()
 {
   bool r = true;      // test result
 
-  r &= testSpectrogramResynthesis(128, 64, 500, 128);
+  // last parameter not yet used in function..
+
+  r &= testSpectrogramResynthesis(128, 64, 500, 128); // H = B/2
+  r &= testSpectrogramResynthesis(128, 32, 500, 128); // H = B/4
 
   return r;
 }
