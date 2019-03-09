55a85d0e339b5115dbeff4dfea8b8a15305981ea
Robin Schmidt <robin@rs-met.com>
2019-03-09 14:10:23 +0100

    added stub for new rs_testing juce module

---
+++ Libraries/JUCE/modules/rs_testing/rs_testing.cpp
@@ -0,0 +1,9 @@
+#ifdef RS_TESTING_H_INCLUDED
+/* When you add this cpp file to your project, you mustn't include it in a file where you've
+already included any other headers - just put it inside a file on its own, possibly with your config
+flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
+header files that the compiler may be using. */
+#error "Incorrect use of JUCE cpp file"
+#endif
+
+#include "rs_testing.h"
---
+++ Libraries/JUCE/modules/rs_testing/rs_testing.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               rs_testing
+  vendor:           RS-MET
+  version:          0.0.1
+  name:             Test Tools
+  description:      Test tools for rapt and rosic modules
+  website:          http://www.rs-met.com
+  license:          Custom
+
+  dependencies:     rapt, rosic
+  OSXFrameworks:
+  iOSFrameworks:
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+#ifndef RS_TESTING_H_INCLUDED
+#define RS_TESTING_H_INCLUDED
+
+#include <rapt/rapt.h>
+#include <rosic/rosic.h>
+
+#endif // #ifndef RS_TESTING_H_INCLUDED
