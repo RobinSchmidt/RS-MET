f5a690c7589e10dbdaac21807c0029f34b682f19
Robin Schmidt <robin@rs-met.com>
2019-09-30 09:50:00 +0200

    removed obsolete includes

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/MathUnitTests.h
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/MathUnitTests.h
@@ -2,29 +2,6 @@
 #define RAPT_MATHUNITTESTS_H
 
 
-// get rid of these includes:
-
-//#include "../../Shared/Shared.h"
-#include "Math/LinearAlgebraUnitTests.h"
-#include "Math/PolynomialUnitTests.h"
-#include "Math/VectorUnitTests.h"
-#include "Math/MatrixUnitTests.h"
-#include "Math/MiscMathUnitTests.h"
-
-// under construction:
-#include "Math/DifferentialEquationTests.h"
-#include "Math/IntegerFunctionTests.h"
-#include "Math/MoebiusTransformTests.h"
-#include "Math/NumberTheoryTests.h"
-#include "Math/RealFunctionTests.h"
-#include "Math/TransformsTests.h"
-//#include "Math/StatisticsTests.h"  // there is no such file - why?
-#include "Math/Point2DTests.h"
-#include "Math/Polygon2DTests.h"
-#include "Math/Triangle2DTests.h"
-
-#include "Math/MultiArrayTests.h"
-
 bool coordinateMapperUnitTest();
 bool correlationUnitTest();
 bool fitRationalUnitTest();
