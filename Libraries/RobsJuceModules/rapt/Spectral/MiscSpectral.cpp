8210fde4a4b0cc2fe5a6de6bb8559da1c1eb37a7
Robin Schmidt <robin@rs-met.com>
2019-12-12 18:15:50 +0100

    prepared for passing two indices to rsExpDecayParameters

--- Libraries/RobsJuceModules/rapt/Spectral/MiscSpectral.cpp
+++ Libraries/RobsJuceModules/rapt/Spectral/MiscSpectral.cpp
@@ -3,8 +3,14 @@
 template<class T>
 std::vector<T> rsExpDecayTail(const rsSinusoidalPartial<T>& partial, int spliceIndex, T sampleRate)
 {
-  return rsExpDecayTail((int)partial.getNumDataPoints(),
-    &partial.getTimeArray()[0], &partial.getAmplitudeArray()[0], 
+  std::vector<T> t = partial.getTimeArray();
+  std::vector<T> a = partial.getAmplitudeArray();
+  int numFrames = (int) partial.getNumDataPoints();
+  int maxIndex  = RAPT::rsArray::maxIndex(&a[0], numFrames);
+
+  // todo: pass the maxIndex into the function call below:
+
+  return rsExpDecayTail(numFrames, &t[0], &a[0], 
     spliceIndex, sampleRate, partial.getFreq(spliceIndex), partial.getPhase(spliceIndex));
   // maybe instead of using the instantaneous frequency at the splice index, we should use the 
   // average frequency ...hmm....well...that seems suitable for modal modeling of the whole partial
