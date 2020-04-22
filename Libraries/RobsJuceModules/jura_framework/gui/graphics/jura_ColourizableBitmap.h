5736dc2c577f71b9a8bf44f36d16d6363d9ed2c9
Robin Schmidt <robin@rs-met.com>
2020-04-22 19:48:34 +0200

    added comments to ColourizableBitmap

--- Libraries/RobsJuceModules/jura_framework/gui/graphics/jura_ColourizableBitmap.h
+++ Libraries/RobsJuceModules/jura_framework/gui/graphics/jura_ColourizableBitmap.h
@@ -1,11 +1,19 @@
 #ifndef jura_ColourizableBitmap_h
 #define jura_ColourizableBitmap_h
 
-/** This class ...needs to be documented
+/** This class ...needs to be documented...it's actually used as an alpha-mask to composit the 
+shape stored in the alpha-mask with some existing juce::Image in a particular color - i.e. the 
+desired shape (stored in the alpha-mask) is composited in the desired color with the existing 
+image. This is used as basis for the pixel-fonts.
 
 \todo try to get rid of inheriting from juce::Image - newer versions of juce disallow this by 
-declaring the image class final. I had to hack the juce library and remove this final declaration 
-in my copy of the juce source tree - which is a dirty and hacky thing to do. */
+declaring the Image class final. I had to hack the juce library and remove this final declaration 
+in my copy of the juce source tree - which is a dirty and hacky thing to do. ..but how? Maybe use 
+rsMatrix or rsImage as baseclass in place of juce::Image and in renderImage, pass the juce::Image 
+onto which we should render? Maybe during transition, provide two renderImage functions - one 
+without parameters (the current one) and one with a reference to a juce::Image representing the 
+image to render on. Then transition to using the second one everywhere, then remove the first, 
+then replace the juce::Image baseclass with rsImage or something. */
 
 class JUCE_API ColourizableBitmap : public juce::Image
 {
