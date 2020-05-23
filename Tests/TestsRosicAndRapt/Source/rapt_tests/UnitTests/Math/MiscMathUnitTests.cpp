9a982d39841df085a46db62f0095ce5ea98fef7d
Robin Schmidt <robin@rs-met.com>
2020-05-23 12:34:44 +0200

    added code to testNumericJacobian

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/Math/MiscMathUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/Math/MiscMathUnitTests.cpp
@@ -420,6 +420,7 @@ static void jacobian(const F& f, T* x, int N, T* pJ, const T* h)
   for(int m = 0; m < M; m++)
     NumDif::gradient(f[m], x, N, &pJ[m*N], h);
 }
+// is this the actual jacobian or the transposed jacobian?
 
 bool testNumericJacobian()
 {
@@ -428,6 +429,7 @@ bool testNumericJacobian()
   // create a function from R^2 to R^3, i.e. a parametric surface
 
   using Func = std::function<double(double*)>;
+  using Mat   = rsMatrix<double>;
 
   int N = 2;  // number of input dimensions
   int M = 3;
@@ -450,19 +452,35 @@ bool testNumericJacobian()
     return x*x * y*y - 2*x*y; 
   };
 
+
+  // the Jacobian is:
+  // df1/dx = 2*x   *   y^3      df1/dy = x^2   * 3*y^2
+  // df2/dx = 6*x^2 * 3*y^2      df2/dy = 2*x^3 * 6*y
+  // df3/dx = 2*x * y^2 - 2*y    df3/dy = x^2 * 2*y - 2*x
+
+  double x = 7, y = 5;
+
+  Mat Ja(3, 2);  // analytical Jacobian
+  Ja(0, 0) = 2*x * y*y*y; 
+  Ja(0, 1) = x*x * 3*y*y;
+  Ja(1, 0) = 6*x*x * 3*y*y;
+  Ja(1, 1) = 2*x*x*x * 6*y;
+  Ja(2, 0) = 2*x * y*y - 2*y;
+  Ja(2, 1) = x*x * 2*y - 2*x;
+
   std::vector<Func> f({f1,f2,f3});
 
-  rsMatrix<double> J(3, 2);
-  double *pJ = J.getDataPointer();
+  Mat Jn(3, 2);  // numerical Jacobian
+  double *pJ = Jn.getDataPointer();
 
-  std::vector<double> x({1,2,3});
+  std::vector<double> v({x,y});
 
   double hx = pow(2, -18);    // from 2^-18, maxErr in the gradient becomes 0
   double hy = hx/2;
   double hz = hx/4;
   double h[3] = {hx, hy, hz};  // h as array
 
-  jacobian(f, &x[0], N, pJ, h);
+  jacobian(f, &v[0], N, pJ, h);
 
   return r;
 }
