33090c1b89e3500d5830418c485e0c04e425872c
Robin Schmidt <robin@rs-met.com>
2020-07-26 15:46:35 +0200

    added test to HeapFilter.txt

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/FilterUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/FilterUnitTests.cpp
@@ -333,7 +333,7 @@ bool testMovingQuantileModulation()
 {
   bool r = true;
 
-  int maxLength = 6;
+  int maxLength = 8;
   int N = 200;           // number of samples
 
   // create vector of settings, each with a timestamp:
@@ -349,7 +349,8 @@ bool testMovingQuantileModulation()
   //std::vector<Settings> settings ={ {0, 5, 7}, {50, 6, 6}, {150, 5, 7} };
   //std::vector<Settings> settings ={ {0, 3, 1}, {20, 2, 2}, {40, 1, 3}, {60, 3, 1} };
   //std::vector<Settings> settings ={ {0, 1, 3}, {6, 2, 2} };
-  std::vector<Settings> settings ={ {0, 2, 4}, {9, 3, 3} };
+  //std::vector<Settings> settings ={ {0, 2, 4}, {9, 3, 3} };
+  std::vector<Settings> settings ={ {0, 3, 5}, {9, 4, 4} };
   //std::vector<Settings> settings ={ {0, 1, 4}, {40, 2, 3} };
   //std::vector<Settings> settings ={ {0, 1, 2}, {20, 2, 1} };
   //std::vector<Settings> settings ={ {0, 1, 4}, {20, 3, 2} };
