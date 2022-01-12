fa36ee7cfd6288e48810f04039f53fb92f3c2dda
Robin Schmidt <robin@rs-met.com>
2022-01-12 15:11:41 +0100

    Update GeneratorUnitTests.cpp

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/GeneratorUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/GeneratorUnitTests.cpp
@@ -2442,44 +2442,38 @@ bool samplerOverloadTest()
   se.setRegionSetting(0, 0, OC::cutoffN, 2000.f, 2);
   se.setRegionSetting(0, 0, OC::cutoffN, 3000.f, 3);
 
+  // Helper function that triggers a note and returns true, iff after triggering, the number of 
+  // active RegionPlayers in the engine is equal to expectedLayers
+  auto testNote = [&](int note, int expectedLayers)
+  {
+    se.handleMusicalEvent(Ev(EvTp::noteOn, note, 127));
+    return se.getNumActiveLayers() == expectedLayers;
+  };
+
   // Try to trigger notes. The first and second should play but the third should not due to not 
   // enough filters available:
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 60, 127));
-  ok &= se.getNumActiveLayers() == 1;
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 61, 127));
-  ok &= se.getNumActiveLayers() == 2;
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 62, 127)); // triggers filter overload
-  ok &= se.getNumActiveLayers() == 2;               // the 3rd region should not start
+  ok &= testNote(60, 1);
+  ok &= testNote(61, 2);
+  ok &= testNote(62, 2);
 
-  // Now put also a cutoff settings for 3 filters into the group. In normal mode, this should make
+  // Now put also cutoff settings for 3 filters into the group. In normal mode, this should make
   // no difference because the group merely defines a fallback value:
   se.reset();
   se.setGroupSetting(0, OC::cutoffN, 1100.f, 1);
   se.setGroupSetting(0, OC::cutoffN, 1200.f, 2);
   se.setGroupSetting(0, OC::cutoffN, 1300.f, 3);
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 60, 127));
-  ok &= se.getNumActiveLayers() == 1;
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 61, 127));
-  ok &= se.getNumActiveLayers() == 2;
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 62, 127));
-  ok &= se.getNumActiveLayers() == 2;  
+  ok &= testNote(60, 1);
+  ok &= testNote(61, 2);
+  ok &= testNote(62, 2);
 
   // Now switch to busMode in which case the group sub-bus needs an additional filter so the
   // first not already consumes 6 filters. A second note should not play:
   se.setBusMode(true);  // calls reset
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 60, 127));
-  ok &= se.getNumActiveLayers() == 1;
-  se.handleMusicalEvent(Ev(EvTp::noteOn, 61, 127));
-  ok &= se.getNumActiveLayers() == 1;
-  // fails!
-
-
-
+  ok &= testNote(60, 1);
+  ok &= testNote(61, 1);
 
 
   // ToDo: 
-  // -write a helper function to reduce boilerplate that we can call like
-  //    ok &= testNote(60, 1);  // 60 is the nore, 1 the expected number of activel layers
   // -Test running out of RegionPlayers and GroupPlayers
   // -Try it with multiple groups where we run out when a new group must start but not not when
   //  a new region within an already playing group must start
