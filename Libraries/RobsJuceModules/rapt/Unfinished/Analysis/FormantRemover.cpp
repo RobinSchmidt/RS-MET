dbd94ff97d0144dabb95634e4f79027c529b102f
Robin Schmidt <robin@rs-met.com>
2022-12-04 08:14:06 +0100

    Update FormantRemover.cpp

--- Libraries/RobsJuceModules/rapt/Unfinished/Analysis/FormantRemover.cpp
+++ Libraries/RobsJuceModules/rapt/Unfinished/Analysis/FormantRemover.cpp
@@ -13,3 +13,27 @@ void rsFormantRemover<TSig, TPar>::reset()
   pastOut = 0.0;
   rsLinearPredictor<TSig, TPar>::reset();
 }
+
+
+/*
+
+Ideas:
+
+Maybe try to implement a spectral dynamics algorithm based on adaptive filtering. See:
+
+https://www.kvraudio.com/forum/viewtopic.php?t=590275
+https://oeksound.com/plugins/soothe2/
+
+Maybe use a filtered input signal to drive the analysis while still acting on the original input. 
+The filter can be lowpass/peak/bandpass/highpass etc. to make the algorithm more sensivtive to 
+certain frequency ranges. Maybe that's what soothe does with these EQ-style filter settings. We 
+need to run the LMS algo on a pre-filtered signal and then run the resulting filter also on the 
+original input. It's like side-chaining where the side-chain signal is a pre-filtered input. 
+Attack/Release parameters can be related to learn- and forget-rate. Try using more advanced 
+adaptive filter structures like recursive least squares (RLS), gradient adaptive lattice (GAL), 
+least squares lattice (LSL), fast transversal filter (FTF), maybe even adaptive IIR filters.
+
+
+
+
+*/
