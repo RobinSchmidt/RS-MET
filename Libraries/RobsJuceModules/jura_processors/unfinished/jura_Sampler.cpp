052bc0e516220be8338c37e824a11f00934e5a2b
Robin Schmidt <robin@rs-met.com>
2023-05-22 15:17:53 +0200

    Update jura_Sampler.cpp

--- Libraries/RobsJuceModules/jura_processors/unfinished/jura_Sampler.cpp
+++ Libraries/RobsJuceModules/jura_processors/unfinished/jura_Sampler.cpp
@@ -1796,7 +1796,8 @@ editor content is not in sync with the lastvalidSfz? That may be good solution
 Bugs:
 -On ctrl-click on the slider, the slider itself switches back to the default value but it's not 
  reflected in the code or tree. SfzOpcodeWidgetSet::rSliderValueChanged is not called back on 
- ctrl-click. Check implementation of RSlider::mouseDown
+ ctrl-click. Check implementation of RSlider::mouseDown ..indeed we call setToDefaultValue(false);
+ ...why?
 -For the modulation connections, we don't get any sliders to appear
  -write some sort of unit test for GUI interactions in the TestAppJURA and then use that to find 
   and fix this bug
