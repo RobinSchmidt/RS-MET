b5ccd22e5d49e0e538edb00ee31a046fd9b9be71
Robin Schmidt <robin@rs-met.com>
2023-07-13 13:22:03 +0200

    Update RootFinder.cpp

--- Libraries/RobsJuceModules/rapt/Math/Numerics/RootFinder.cpp
+++ Libraries/RobsJuceModules/rapt/Math/Numerics/RootFinder.cpp
@@ -65,13 +65,18 @@ T rsRootFinder<T>::falsePosition(const std::function<T(T)>& f, T xL, T xR, T y)
 }
 
 
-// todo: implement the modified false position method describen in Hamming's "Numerical Methods 
-// for Scientists and Enginners (2nd Ed)", pages 65-67 - should converge better for functions with
-// flat regions
-// see also: https://en.wikipedia.org/wiki/False_position_method#The_Illinois_algorithm
-// "When the new y-value has the same sign as the previous one, meaning that the data point before the 
-// previous one will be retained, the Illinois version halves the y-value of the retained data point."
 /*
+
+
+
+ToDo: 
+-Implement the modified false position method describen in Hamming's "Numerical Methods 
+ for Scientists and Enginners (2nd Ed)", pages 65-67 - should converge better for functions with
+ flat regions see also: https://en.wikipedia.org/wiki/False_position_method#The_Illinois_algorithm
+ "When the new y-value has the same sign as the previous one, meaning that the data point before 
+  the previous one will be retained, the Illinois version halves the y-value of the retained data
+  point."
+
 template<class T>
 T rsRootFinder<T>::modifiedFalsePosition(std::function<T(T)>& f, T xL, T xR, T y)
 {
@@ -95,9 +100,32 @@ T rsRootFinder<T>::modifiedFalsePosition(std::function<T(T)>& f, T xL, T xR, T y
   rsError("rsRootFinder::modifiedFalsePosition failed to converge");
   return xM;
 }
+
+
+ToDo: 
+-Maybe factor out common stuf...maybe calling a function getNewEstimate(xL, fL, xM, fM, xR, fR) 
+ and call it as xM = getNewEstimate(xL, fL, ...) - such a structure can also accomodate for Brent
+ algorithm later (fits (inverse) parabola through 3 points)
+-Let the user pass the maximum number of iterations and (relative) tolerance. I think, we should 
+ always work with relative rather than absolute tolerance because it just makes more sense.
+-Implement Newton iteration with and without Schroeder's modification. The regular Newton 
+ iteration uses the update rule:
+   x[n+1] = x[n] + d[n]   where   d[n] = - f(x[n]) / f'(x[n])
+ This rule has slow convergence when the root has a multiplicity m > 1. Schroeder's modification 
+ fixes this problem by using the update rule:
+   x[n+1] = x[n] + m*d[n]
+ with d[n] defined as before. We could let the user pass the multiplicity m for cases where it is 
+ known. If m is unknown, we could let the algorithm have a current estimate m[n] (initialized to 1) 
+ and in each iteration, try 3 tentative update steps using m-1, m, m+1 and then actually use the
+ step that brought us closest to zero and use as m[n+1] the value that gave rise to this best 
+ result. We could also return our final estimate of m to the caller as estimate for the 
+ multiplicity of the root. Maybe when m[n] = 1, don't try m-1. It would be useless because then
+ we would do x[n+1] = x[n] + 0*d[n]. But maybe just do it anyway to keep the logic simple. It 
+ isn't expensive. See Numerik (Meister, Sonar), page 249.
+ Q: What other conditions besides root multiplicity can slow down convergence? I think, having a 
+ flat region around the current root estimate (but not necessarily around the root itself) can 
+ shoot the next estimate far away. IIRC, Numerical Recipies has some discussion about this (not 
+ sure though). Can this issue be fixed by some modification, too?
+   
+
 */
-
-// todo: factor out common stuf...maybe calling a function getNewEstimate(xL, fL, xM, fM, xR, fR) 
-// and call it as xM = getNewEstimate(xL, fL, ...) - such a structure can also accomodate for brent
-// algorithm later (fits (inverse) parabola through 3 points)
-
