11046559d474b7e6de75076c6f82921af54e3c43
Robin Schmidt <robin@rs-met.com>
2024-11-10 07:12:23 +0100

    moved rsStateVariableFilterMystran2

--- Libraries/RobsJuceModules/rs_testing/Prototypes/FilterDesign/MiscFilters.h
+++ Libraries/RobsJuceModules/rs_testing/Prototypes/FilterDesign/MiscFilters.h
@@ -150,3 +150,142 @@ TSig rsStateVariableFilterChain<TSig, TCoef>::getSample(TSig in)
 }
 
 //=================================================================================================
+
+/** Subclass of rsStateVariableFilterMystran that extends it by a general setup() function that
+takes a mode parameter and then dispatches to the different setup functions. It also adds some
+inquiry functions that retrieve the design parameter from the coefficients. That's probably a
+rather useless functionality - if something like that is desired, it would make more sense to
+just store it in additional member variables in some wrapper subclass. */
+
+template<class TSig, class TPar>
+class rsStateVariableFilterMystran2 : public rsStateVariableFilterMystran<TSig, TPar>
+{
+
+public:
+
+  //-----------------------------------------------------------------------------------------------
+  // \name Setup
+
+  /** Enumeration of the available filter modes. */
+  enum Mode
+  {
+    Bypass,
+    Lowpass,
+    Highpass,
+    BandpassSkirt,
+    BandpassPeak,
+    Bandstop,
+    Allpass,
+    Bell,
+    LowShelf,
+    HighShelf,
+
+    NumModes
+  };
+
+  void setup(Mode mode, TPar w, TPar Q, TPar A = TPar(1))
+  {
+    switch(mode)
+    {
+    case Mode::Bypass:        setupBypass();               break;
+    case Mode::Lowpass:       setupLowpass(      w, Q);    break;
+    case Mode::Highpass:      setupHighpass(     w, Q);    break;
+    case Mode::BandpassSkirt: setupBandpassSkirt(w, Q);    break;
+    case Mode::BandpassPeak:  setupBandpassPeak( w, Q);    break;
+    case Mode::Bandstop:      setupBandstop(     w, Q);    break;
+    case Mode::Allpass:       setupAllpass(      w, Q);    break;
+    case Mode::Bell:          setupBell(         w, Q, A); break;
+    case Mode::LowShelf:      setupLowShelf(     w, Q, A); break;
+    case Mode::HighShelf:     setupHighShelf(    w, Q, A); break;
+    default:
+    {
+      rsError("Unknown filter type in rsStateVariableFilterMystran::setup");
+      a0 = a1 = a2 = 0;
+      g = 0;
+      gpr = 0;
+      scl = 0;
+    };
+    }
+  }
+
+  //-----------------------------------------------------------------------------------------------
+  // \name Inquiry
+
+  bool isLowpass()       const { return a0 == 1 && a1 == 0 && a2 == 0; }
+  bool isHighpass()      const { return a0 == 0 && a1 == 0 && a2 == 1; }
+  bool isBandpass()      const { return a0 == 0 && a1 >  0 && a2 == 0; }
+  bool isBandpassSkirt() const { return a0 == 0 && a1 == 1 && a2 == 0; }
+  bool isBandpassPeak()  const { return a0 == 0 && a1 != 1 && a2 == 0; }        // a1 = r = 1/Q
+  bool isBandstop()      const { return a0 == 1 && a1 == 0 && a2 == 1; }
+  bool isAllpass()       const { return a0 == 1 && a1 <  0 && a2 == 1; }        // a1 = -1/Q
+  bool isBell()          const { return a0 == 1 && a1 >  0 && a2 == 1; }        // a1 = A^2/Q
+  bool isShelf()         const { return isLowShelf() || isHighShelf(); }
+  bool isLowShelf()  const { return /*a0 >  0 &&*/ a0 != 1 && a1 >  0 && a2 == 1; } // a0=A^2, a1=A/Q
+  bool isHighShelf() const { return a0 == 1 && a1 >  0 && /* a2 >  0 && */ a2 != 1; } // a2=A^2, a1=A/Q
+
+
+
+  // ToDo: check, if we really need the a0 > 0 condition for LS and a2 > 0 condition for HS
+
+  // I think there's an edge case of Q = 1 where constant peak and constant skirt bandpasses are
+  // indistinguishable. I think, it this case, both should return true - but they currently don't
+  // I think. We need to check that in any case one and only one of them returns true, i.e. that
+  // the conditions are disjoint or mutually exclusive - except in edge cases maybe.
+
+
+  TPar getOmega() const
+  {
+    if(isLowShelf())
+    {
+      TPar r = gpr - g;
+      TPar A = a1 / r;
+      return 2*atan(g*sqrt(A));        // g = tan(w/2) / sqrt(A);
+    }
+    else if(isHighShelf())
+    {
+      TPar r = gpr - g;
+      TPar A = a1 / r;
+      return 2*atan(g/sqrt(A));        // g = tan(w/2) * sqrt(A)
+    }
+    else
+      return 2*atan(g);                // g = tan(w/2)
+  }
+
+  TPar getQualityFactor() const
+  {
+    if(isBell())
+    {
+      return sqrt(1/(a1*(gpr-g)));
+
+      // We have 3 equations involving r, Q, A: (1) r = 1/(Q*A), (2) gpr = g + r, (3) a1 = A^2 * r
+      // where the knowns are  gpr, g, a1  and the unknowns are r, Q, A. These equations can be 
+      // grabbed directly from the code in setupBell(). Sage can solve this simple nonlinear 
+      // system of equations for us with the following code:
+      //
+      //  var("gpr g a1 r  Q A")
+      //  e1 = r   == 1/(Q*A)
+      //  e2 = gpr == g + r
+      //  e3 = a1  == A^2 * r
+      //  solve([e1,e2,e3],[r,Q,A])
+      //
+      // Picking the 1st solution and manually making it prettier gives the result above.
+    }
+    else
+      return 1 / (gpr - g);  // gpr = g + r  ->  r = gpr - g = 1/Q
+  }
+
+  TPar getBellGain() const
+  {
+    rsAssert(isBell(), "Calling this function only makes sense for bell filters");
+    TPar Q = sqrt(1/(a1*(gpr-g)));
+    return 1 / (Q*(gpr-g));
+  }
+
+  TPar getShelfGain() const
+  {
+    rsAssert(isShelf(), "Calling this function only makes sense for shelf filters");
+    return a1 / (gpr - g);
+  }
+
+};
+
