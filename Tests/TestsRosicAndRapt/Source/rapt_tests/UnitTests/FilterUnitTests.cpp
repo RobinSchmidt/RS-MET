9e496b342db92499a6600eb7cbce7e64740c6821
Robin Schmidt <robin@rs-met.com>
2024-11-11 15:32:14 +0100

    Update FilterUnitTests.cpp

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/FilterUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/FilterUnitTests.cpp
@@ -1408,8 +1408,9 @@ bool stateVariableFilterUnitTest4()
 
   designBiquad(Mode::Lowpass, 1000, 5, 0, b0, b1, b2, a1, a2);
   svf.setupLowpass(2*PI*1000/sampleRate, 5);
-  svf.getBiquadNumeratorCoeffsLP(&b0s, &b1s, &b2s);
-  svf.getBiquadDenominatorCoeffs(      &a1s, &a2s);
+  //svf.getBiquadNumeratorCoeffsLP(&b0s, &b1s, &b2s);
+  //svf.getBiquadDenominatorCoeffs(      &a1s, &a2s);
+  svf.getBiquadCoeffs(&b0s, &b1s, &b2s, &a1s, &a2s); 
   ok &= rsIsCloseTo( b0, b0s, tol);
   ok &= rsIsCloseTo( b1, b1s, tol);
   ok &= rsIsCloseTo( b2, b2s, tol);
@@ -1420,8 +1421,9 @@ bool stateVariableFilterUnitTest4()
 
   designBiquad(Mode::BandpassSkirt, 1000, 5, 0, b0, b1, b2, a1, a2);
   svf.setupBandpassSkirt(2*PI*1000/sampleRate, 5);
-  svf.getBiquadNumeratorCoeffsBP(&b0s, &b1s, &b2s);
-  svf.getBiquadDenominatorCoeffs(      &a1s, &a2s);
+  //svf.getBiquadNumeratorCoeffsBP(&b0s, &b1s, &b2s);
+  //svf.getBiquadDenominatorCoeffs(      &a1s, &a2s);
+  svf.getBiquadCoeffs(&b0s, &b1s, &b2s, &a1s, &a2s); 
   ok &= rsIsCloseTo( b0, b0s, tol);
   ok &= rsIsCloseTo( b1, b1s, tol);
   ok &= rsIsCloseTo( b2, b2s, tol);
@@ -1430,14 +1432,18 @@ bool stateVariableFilterUnitTest4()
 
   designBiquad(Mode::Highpass, 1000, 5, 0, b0, b1, b2, a1, a2);
   svf.setupHighpass(2*PI*1000/sampleRate, 5);
-  svf.getBiquadNumeratorCoeffsHP(&b0s, &b1s, &b2s);
-  svf.getBiquadDenominatorCoeffs(      &a1s, &a2s);
-  svf.getBiquadCoeffs(&b0s, &b1s, &b2s, &a1s, &a2s);  // Makes it fail!
+  //svf.getBiquadNumeratorCoeffsHP(&b0s, &b1s, &b2s);
+  //svf.getBiquadDenominatorCoeffs(      &a1s, &a2s);
+  svf.getBiquadCoeffs(&b0s, &b1s, &b2s, &a1s, &a2s); 
   ok &= rsIsCloseTo( b0, b0s, tol);
   ok &= rsIsCloseTo( b1, b1s, tol);
   ok &= rsIsCloseTo( b2, b2s, tol);
   ok &= rsIsCloseTo(-a1, a1s, tol);
   ok &= rsIsCloseTo(-a2, a2s, tol);
+
+
+
+
 
   designBiquad(Mode::Bell, 1000, 5.0, 6.0, b0, b1, b2, a1, a2);
   svf.setupBell(2*PI*1000/sampleRate, 5.0, 6.0);
