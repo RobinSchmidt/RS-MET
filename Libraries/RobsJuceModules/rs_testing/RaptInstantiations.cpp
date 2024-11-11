144136c6f4ee505629aa326abbbed8b20882c672
Robin Schmidt <robin@rs-met.com>
2024-11-11 17:35:54 +0100

    moved getBiquadNumeratorCoeffs into .cpp file

--- Libraries/RobsJuceModules/rs_testing/RaptInstantiations.cpp
+++ Libraries/RobsJuceModules/rs_testing/RaptInstantiations.cpp
@@ -575,12 +575,13 @@ template void RAPT::rsBiDirectionalFilter::applyLowpass(
 template class RAPT::rsCrossOver4Way<double, double>;
 //template class RAPT::rsQuantileFilter<double>;
 template class RAPT::rsHilbertFilter<double, double>;
-
+template class RAPT::rsStateVariableFilterMystran<double, double>;
 
 template class rsStateVariableFilterChain<double, double>; // not yet in RAPT namespace
 template class rsStateVariableFilterChain<float, float>; 
 template class rsStateVariableFilterChain<float, double>; 
 template class rsStateVariableFilterChain<long double, long double>; 
+
 
 
 // Physics:
