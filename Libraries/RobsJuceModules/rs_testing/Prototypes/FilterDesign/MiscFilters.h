a910d3abadec4af5be55b07cbf73e1dbe7724f4c
Robin Schmidt <robin@rs-met.com>
2024-11-20 13:47:57 +0100

    added stateVariableToStateSpace

--- Libraries/RobsJuceModules/rs_testing/Prototypes/FilterDesign/MiscFilters.h
+++ Libraries/RobsJuceModules/rs_testing/Prototypes/FilterDesign/MiscFilters.h
@@ -620,3 +620,49 @@ rsMatrix<rsComplex<T>> rsStateSpaceFilter<T>::getTransferFunctionAt(rsComplex<T>
   // our matrices.
 }
 
+
+/** Converts a state variable filter with coefficients g,c,s into a state space form with one input
+and 3 outputs for the highpass, bandpass and lowpass part. The matrices for the state space form 
+are given by:
+
+  A = [1-2gsc     -2gs ]     B = [2gs ]     C = [-sc        -s]     D = [  s]
+      [2g-2ggsc  1-2ggs]         [2ggs]         [1-gsc     -gs]         [ gs]
+                                                [g-ggsc  1-ggs]         [ggs]  
+
+See Notes/StateVariableFilter.txt for derivation of these formulas. The caller needs to pass 
+pre-allocated objects of type rsMatrixView of the right shapes. The required shapes are: 
+A: 2x2, B: 2x1, C: 3x2, D: 3x1. */
+template<class T>
+void stateVariableToStateSpace(T g, T c, T s, 
+  rsMatrixView<T>* A, rsMatrixView<T>* B, rsMatrixView<T>* C, rsMatrixView<T>* D)
+{
+  T gs  = g*s;
+  T ggs = g*gs;
+
+  // 2x2 state transition matrix:
+  rsAssert(A->hasShape(2,2));
+  (*A)(0,0) =  1  -2*gs*c;
+  (*A)(0,1) =     -2*gs;
+  (*A)(1,0) =  2*g-2*ggs*c;
+  (*A)(1,1) =  1  -2*ggs;
+
+  // 2x1 input matrix:
+  rsAssert(B->hasShape(2,1));
+  (*B)(0,0) =  2*gs;
+  (*B)(1,0) =  2*ggs;
+
+  // 3x2 output matrix:
+  rsAssert(C->hasShape(3,2));
+  (*C)(0,0) = -s*c;
+  (*C)(0,1) = -s;
+  (*C)(1,0) = 1-gs*c;
+  (*C)(1,1) =  -gs;
+  (*C)(2,0) = g-ggs*c;
+  (*C)(2,1) = 1-ggs;
+
+  // 3x1 feedaround matrix:
+  rsAssert(D->hasShape(3,1));
+  (*D)(0,0) =   s;
+  (*D)(1,0) =  gs;
+  (*D)(2,0) = ggs;
+}
