840c8680be782f32fcdfa3de1e1bb7762a6696e2
Robin Schmidt <robin@rs-met.com>
2024-12-04 01:31:04 +0100

    Update StateVariableFilter.cpp

--- Libraries/RobsJuceModules/rapt/Filters/Musical/StateVariableFilter.cpp
+++ Libraries/RobsJuceModules/rapt/Filters/Musical/StateVariableFilter.cpp
@@ -13,9 +13,14 @@ void rsStateVariableFilter<TSig, TPar>::setupFromBiquad(
   TPar r  = 2*(a2 - 1) / (T*S);
   if(T >= 0)
   {
-    rsError("The formulas work only for T < 0.");          // For T > 0, S and r are NaN
-    setupMuted();                                          // We produce muted output in this case.
+    rsError("The formulas work only for T < 0.");
+    setupMuted();
     return;
+    // For T > 0, S and r are NaN because we are trying to take a square root of a negative number. 
+    // If this happens, it means your biquad coeffs were unstable. We produce muted output in this 
+    // case. It doesn't happen for all unstable biquads, though. For some unstable biquads, the 
+    // formulas still work. In these cases, we'll just use them anyway and you'll get an unstable 
+    // SVF. I'm not sure, if that behavior is best, though.
   }
 
   // Compute final coefficients:
