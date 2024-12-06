96edce125ddee3cb29269ae52fd73245fa4f0e64
Robin Schmidt <robin@rs-met.com>
2024-12-06 20:39:11 +0100

    added Allpasses.h/cpp

---
+++ Libraries/RobsJuceModules/rapt/Filters/Musical/Allpasses.h
@@ -0,0 +1,11 @@
+#ifndef RAPT_ALLPASSES_H_INCLUDED
+#define RAPT_ALLPASSES_H_INCLUDED
+
+
+// This file is currently a stub. It should be filled with some classes that are currently located
+// in ReverbStuff.h in the Prototypes folder. This includes allpass chains and nested allpass 
+// structures that are used as building blocks for reverbs. Maybe we should also drag the code from
+// rosic::FlatZapper in and call it rsAllpassDisperser or something like that. Maybe we should also
+// implement Thiran allpass filters here. They are used for allpass interpolation.
+
+#endif
