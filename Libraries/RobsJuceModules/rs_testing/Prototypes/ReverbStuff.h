7be998a897f0f0736b24968f2b407cc2287cb964
Robin Schmidt <robin@rs-met.com>
2024-12-09 12:46:22 +0100

    Update ReverbStuff.h

--- Libraries/RobsJuceModules/rs_testing/Prototypes/ReverbStuff.h
+++ Libraries/RobsJuceModules/rs_testing/Prototypes/ReverbStuff.h
@@ -883,8 +883,8 @@ TSig rsDampedAllpassCombNaive<TSig, TPar>::getSample(TSig in)
 template<class TSig, class TPar>
 TSig rsDampedAllpassCombNaive<TSig, TPar>::getSampleComb(TSig in)
 {
-  //out = mainDelay.getSample(in + k * damper.getSample(out));
-  out = damper.getSample(in + k * mainDelay.getSample(out));
+  out = mainDelay.getSample(in + k * damper.getSample(out));
+  //out = damper.getSample(in + k * mainDelay.getSample(out));
   return out;
 
   // In the dampedAllpassComb1() experiment where I derived all of this, I actually use a negative
@@ -1058,8 +1058,8 @@ TSig rsDampedAllpassComb<TSig, TPar>::getSample(TSig in)
 template<class TSig, class TPar>
 TSig rsDampedAllpassComb<TSig, TPar>::getSampleComb(TSig in)
 {
-  //combOut = mainDelay.getSample(in + k * applyDamper(combOut));  // Predelay of M samples
-  combOut = applyDamper(in + k * mainDelay.getSample(combOut));    // No predelay
+  combOut = mainDelay.getSample(in + k * applyDamper(combOut));  // Predelay of M samples
+  //combOut = applyDamper(in + k * mainDelay.getSample(combOut));    // No predelay
   return combOut;
 
   // Notes:
