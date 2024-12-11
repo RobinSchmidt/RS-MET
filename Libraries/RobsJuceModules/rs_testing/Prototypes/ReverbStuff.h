3217deb54ac776946ba9ac447ea3153ecf34678a
Robin Schmidt <robin@rs-met.com>
2024-12-11 04:41:36 +0100

    Update ReverbStuff.h

--- Libraries/RobsJuceModules/rs_testing/Prototypes/ReverbStuff.h
+++ Libraries/RobsJuceModules/rs_testing/Prototypes/ReverbStuff.h
@@ -1157,10 +1157,10 @@ TSig rsDampedAllpassComb<TSig, TPar>::applyCorrector(TSig in)
 
   // Apply the FIR part:
   TSig y = 0;
-  y += r[0] * t;
-  y += r[1] * unitDelay.getSample(t);
-  y += a[1] * corrDelay.readOutputAt(M+1);
-  y += a[0] * corrDelay.readOutputAt(M+2);  // a[0] == 1, but we want to see the pattern
+  y += k * b[1] * t;
+  y += k * b[0] * unitDelay.getSample(t);       // can we use corrDelay.readOutputAt(1)?
+  y +=     a[1] * corrDelay.readOutputAt(M+1);
+  y +=     a[0] * corrDelay.readOutputAt(M+2);  // a[0] == 1, but we want to see the pattern
   corrDelay.writeInputAndUpdate(t);
   return y;
 }
