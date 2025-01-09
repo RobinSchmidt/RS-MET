1d00761cf23100eb415b0eeb9df92b6130517c74
Robin Schmidt <robin@rs-met.com>
2025-01-09 15:36:42 +0100

    Update PolynomialUnitTests.cpp

--- Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/Math/PolynomialUnitTests.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/UnitTests/Math/PolynomialUnitTests.cpp
@@ -3012,6 +3012,20 @@ bool testSparseRationalFunction()
   w2 = rs(z);
   ok &= rsIsCloseTo(w1, w2, tol);
 
+  // Test arithmetic operators:
+  ts = rs * ss;
+  y1 = rs(x) * ss(x);
+  y2 = ts(x);
+  ok &= rsIsCloseTo(y1, y2, tol);
+
+  ts = rs / ss;
+  y1 = rs(x) / ss(x);
+  y2 = ts(x);
+  ok &= rsIsCloseTo(y1, y2, tol);
+
+
+
+
 
 
 
