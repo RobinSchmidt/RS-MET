ae400284f686458d3252bb0ec6e0b011e75b8164
Robin Schmidt <robin@rs-met.com>
2025-01-11 15:15:51 +0100

    moved rsSetupHighDampComplex

--- Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/Experiments/DelayExperiments.cpp
+++ Tests/TestsRosicAndRapt/Source/rosic_tests/PortedFromRSLib/Experiments/DelayExperiments.cpp
@@ -1148,6 +1148,25 @@ void dampedCombAllpass6()
 }
 
 
+
+// This is needed for the case when we want ot have a complex TPar. It's a bit dirty to use 
+// the explicit type double. Maybe it should be a third template parameter TReal or something.
+template<class TSig, class TPar>
+void rsSetupHighDampComplex(rsDampedCombAllpass<TSig, TPar>& flt,
+  int delay, TPar feedback, double dampOmega, double dampGain, bool predelay)
+{
+  double a[2], b[2]; a[0] = 1;
+  rsMake1stOrderHighShelf(dampOmega, dampGain, &b[0], &b[1], &a[1]);
+  TPar A[2], B[2]; 
+  A[0] = TPar(a[0]);
+  A[1] = TPar(a[1]);
+  B[0] = TPar(b[0]);
+  B[1] = TPar(b[1]);
+
+  flt.setup(delay, feedback, 1, B, A, predelay);
+}
+// Clean this up to make it nice!
+
 void dampedCombAllpassComplex()
 {
   // We instantiate rsDampedCombAllpass with a complex datatype for the signals. The feedback gain
