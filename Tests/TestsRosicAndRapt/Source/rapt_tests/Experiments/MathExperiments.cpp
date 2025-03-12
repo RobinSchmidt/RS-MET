d37197143eb74feae5b5895f8f5f715433bca04f
Robin Schmidt <robin@rs-met.com>
2025-03-12 18:40:06 +0100

    Update MathExperiments.cpp

--- Tests/TestsRosicAndRapt/Source/rapt_tests/Experiments/MathExperiments.cpp
+++ Tests/TestsRosicAndRapt/Source/rapt_tests/Experiments/MathExperiments.cpp
@@ -3898,7 +3898,7 @@ double rsSoftBitCrush(double x)
 {
   // Parameters:
   double k = 1.0;  // amount - rename. i think, it's the reciprocal of the quantization interval
-  double h = 1.0;  // hardness
+  double h = 10.0;  // hardness
 
 
   double a  = rsAbs(x);
@@ -3907,16 +3907,27 @@ double rsSoftBitCrush(double x)
   double y  = rsFloor(ka);
   double z  = ka - y;
   double w  = rsLinToLin(z, 0.0, 1.0, -1.0, +1.0);
-  //double v  = tanh(h*w) / h;
-  double v  = tanh(h*w);
-  double u  = rsLinToLin(v, -1.0, +1.0, 0.0, 1.0);
-  double f  = (s/k) * (y + u);
+  // So far, i think it's probably right. But down below, we have some scaling issue, it seems.
 
+
+  //double v  = tanh(h*w) / h;
+  //double v  = tanh(h*w);
+  double v  = tanh(h*w) / tanh(h);
+
+  double u  = rsLinToLin(v, -1.0, +1.0, 0.0, 1.0);
+  //double u  = rsLinToLin(v, -v, +v, 0.0, 1.0);
+
+
+  // I think, this should be ok again:
+  double f  = (s/k) * (y + u);
   return f;
 
   //return x;  // preliminary
 
-  // I think, u needs to be scaled by 1/tanh(..something)
+  // There are jumps for small h (like 1). It looks good for larger h (like 5)
+  // I think, u needs to be scaled by 1/tanh(..something)  maybe tanh(h)
+
+  // ToDo: make a softFloatCrsuh function, integrate them into FuncShaper
 }
 
 void softBitCrush()
