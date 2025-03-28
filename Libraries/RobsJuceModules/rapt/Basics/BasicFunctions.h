61b1e86a13b22510a465cd4669f01d69f34ccaa2
Robin Schmidt <robin@rs-met.com>
2025-03-28 23:04:18 +0100

    Update BasicFunctions.h

--- Libraries/RobsJuceModules/rapt/Basics/BasicFunctions.h
+++ Libraries/RobsJuceModules/rapt/Basics/BasicFunctions.h
@@ -411,40 +411,6 @@ inline unsigned long rsBitReverse(unsigned long number, unsigned long numBits)
 
 
 
-///** Implements the maximum norm for primitive built in types like int, float, double, etc. It's just
-//the absolute value. This serves also as the general fallback implementation. */
-//template<class T>
-//T rsMaxNorm(const T& x)
-//{
-//  return std::abs(x);
-//}
-////template<class TArg, class TNorm>
-////TNorm rsMaxNorm(const TArg& x)
-////{
-////  return (TNorm) std::abs(x);
-////}
-//// Shouldn't we use just a single template parameter, i.e. argument type and return type should match?
-//
-//template<class TArg, class TNorm>
-//TNorm rsMaxNorm(const TArg& x, const TArg& y)
-//{
-//  return rsMax(rsMaxNorm(x), rsMaxNorm(y));
-//}
-//
-///** Implements the maximum norm for std::complex<TReal> where TReal */
-//template<class TReal, class TNorm>
-//TNorm rsMaxNorm(const std::complex<TReal>& z)
-//{
-//  return rsMaxNorm<TReal, TNorm>(std::abs(z.real()), std::abs(z.imag)); 
-//
-//  //return rsMax(std::abs(z.real()), std::abs(z.imag)); 
-//  // Maybe we should use rsMaxNorm rather than rsMax? What if TReal is a simd type, for example? Might 
-//  // rsMax then be unsuitable because the max function should return the underlying scalar type?
-//}
-//
-//
-//
-//
 
 
 
