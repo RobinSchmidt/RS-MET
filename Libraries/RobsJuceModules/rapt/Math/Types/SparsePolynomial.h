b11ef8041ee8831a9ffa01bbcd1878d89776aaae
Robin Schmidt <robin@rs-met.com>
2025-04-01 18:36:35 +0200

    rsMonomial: add () operator for TArg

--- Libraries/RobsJuceModules/rapt/Math/Types/SparsePolynomial.h
+++ Libraries/RobsJuceModules/rapt/Math/Types/SparsePolynomial.h
@@ -47,19 +47,18 @@ public:
 
   /** Evaluates the expression c * x^p at the given x. */
   T evaluateAt(T x) const { return coeff * rsPow(x, T(power)); }
-  // Preliminary. We may want to use rsPowInt for integer exponents. That may be more efficient.
-  // Here, we explicitly first convert the exponent to type T and then call rsPow(T x, T y).
-  // But currently rsPowInt is not suitably defined. It expects two unsigned ints.
+  // get rid
+
 
   //T evaluateAt(T x) const { return coeff * rsPowInt(x, power); }
-  // rsPowInt is defined for x and power being integers. We really need a function where the
-  // base is an arbitrary type and the epxonent is an integer
+
 
   /** Returns the coefficient c in the expression c * x^p where the type of x may be different from
   the type T with which the class is instantiated. Can be used, for example, to evaluate monomials
   with real coefficients at complex arguments. */
   template<class TArg>
   TArg evaluateTyped(TArg z) const { return TArg(coeff) * rsPow(z, TArg(power)); }
+  // get rid
 
 
   //-----------------------------------------------------------------------------------------------
@@ -67,6 +66,18 @@ public:
 
   /** Evaluates the monomial at the given input x. */
   T operator()(T x) const { return evaluateAt(x); }
+  // get rid
+
+
+
+  template<class TArg>
+  T operator()(TArg x) const { return TArg(coeff) * rsPow(x, TArg(power)); }
+  // Preliminary. We may want to use rsPowInt for integer exponents. That may be more efficient.
+  // Here, we explicitly first convert the exponent to type T and then call rsPow(T x, T y).
+  // But currently rsPowInt is not suitably defined. It expects two unsigned ints. But
+  // rsPowInt is currently only defined for x and power being both integers. We really need a 
+  // function where the base is an arbitrary type and the epxonent is an integer
+
 
   /** Returns the negative of this monomial. */
   rsMonomial<T> operator-() const { return rsMonomial<T>(-getCoeff(), getPower()); }
