02f9451bdc8fc457e59688bb1e302d81857f6389
Robin Schmidt <robin@rs-met.com>
2026-05-21 13:00:13 +0200

    Fix warnings. 404 remain.

--- Libraries/RobsJuceModules/romos/Modules/romos_SoundGeneratorModules.cpp
+++ Libraries/RobsJuceModules/romos/Modules/romos_SoundGeneratorModules.cpp
@@ -126,8 +126,11 @@ void BandlimitedImpulseTrain::initialize()
   else                                                                                \
     ampScaler = 0.0;                                                                  \
   double omega    = *in1 * processingStatus.getFreqToOmegaFactor();                   \
-  double absOmega = absFreq * processingStatus.getFreqToOmegaFactor();                \
+  /*double absOmega = absFreq * processingStatus.getFreqToOmegaFactor();                \*/
   double theta = blit->phases[voiceIndex] + TWO_PI * (*in2 + blit->fixedPhaseOffset); \
+
+// ToDo: Maybe try to get rid of that. Macros are a pain to debug.
+
 
 INLINE void BandlimitedImpulseTrain::process(Module *module, double *in1, double *in2, double *out, 
   int voiceIndex)
@@ -359,7 +362,7 @@ double BlitSaw::getDesiredFirstSample(double frequency, double startPhase)
   else
     ampScaler = 0.0;
   double omega    = frequency * processingStatus.getFreqToOmegaFactor();
-  double absOmega = absFreq * processingStatus.getFreqToOmegaFactor();
+  //double absOmega = absFreq * processingStatus.getFreqToOmegaFactor();
   double theta    = TWO_PI * (startPhase);
 
 
